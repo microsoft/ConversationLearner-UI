e10c5708b72b3cb473940a0bc4fcffc3bc8f0633
lucashuet93 <lucashh@microsoft.com>
2017-06-07 12:01:18 -0700

    create skeleton action methods to dispatch consts

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,10 +7,60 @@ export const CREATE_ENTITY = 'CREATE_ENTITY';
 export const CREATE_ACTION = 'CREATE_ACTION';
 export const CREATE_TRAIN_DIALOG = 'CREATE_TRAIN_DIALOG';
 
-export const fetchPosts = () => {
-    const request = axios.get(`${rootURL}/posts${apiKey}`);
+/*-----------------------------------------------------------------------------------------
+--------------------- FETCH CALLS ---------------------------------------------------------
+-----------------------------------------------------------------------------------------*/
+
+export const fetchApplications = () => { 
     return {
-        type: FETCH_POSTS,
+        type: FETCH_APPLICATIONS,
+        payload: request
+    }
+}
+export const fetchAllEntities = () => { 
+    return {
+        type: FETCH_ENTITIES,
+        payload: request
+    }
+}
+export const fetchAllActions = () => { 
+    return {
+        type: FETCH_ACTIONS,
+        payload: request
+    }
+}
+export const fetchTrainDialogs = () => { 
+    return {
+        type: FETCH_TRAIN_DIALOGS,
+        payload: request
+    }
+}
+
+/*-----------------------------------------------------------------------------------------
+--------------------- CREATE CALLS ---------------------------------------------------------
+-----------------------------------------------------------------------------------------*/
+
+export const createBLISApplication = () => { 
+    return {
+        type: CREATE_APPLICATION,
+        payload: request
+    }
+}
+export const createEntity = () => { 
+    return {
+        type: CREATE_ENTITY,
+        payload: request
+    }
+}
+export const createAction = () => { 
+    return {
+        type: CREATE_ACTION,
+        payload: request
+    }
+}
+export const createTrainDialog = () => { 
+    return {
+        type: CREATE_TRAIN_DIALOG,
         payload: request
     }
 }
