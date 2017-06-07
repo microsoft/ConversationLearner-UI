f8c4d05800f46b6b54ad8fdb382cd7335962f2a2
lucashuet93 <lucashh@microsoft.com>
2017-06-06 18:30:35 -0700

    create training ground environment

---
+++ src/TrainingGround.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class TrainingGround extends React.Component{
+    constructor(p){
+        super(p)
+    }
+    render(){
+        return (
+            <div>
+                <h3>TrainingGround</h3>
+            </div>
+        )
+    }
+}
