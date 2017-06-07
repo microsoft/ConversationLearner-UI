d90ebf4af77af4ec6080ef16de230d01e0b1bd47
lucashuet93 <lucashh@microsoft.com>
2017-06-07 13:27:11 -0700

    connect app component to redux and fetch all

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import '../assets/App.css';
 import TrainingGround from '../containers/TrainingGround';
+import { fetchAllActions, fetchAllEntities, fetchApplications, fetchTrainDialogs } from '../actions/fetch';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 class App extends Component {
+  componentDidMount(){
+    this.props.fetchAllActions();
+    this.props.fetchAllEntities();
+    this.props.fetchApplications();
+    this.props.fetchTrainDialogs();
+  }
   render() {
     return (
       <div className="app">
-          <TrainingGround />
+        <TrainingGround />
       </div>
     );
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    fetchAllActions: fetchAllActions,
+    fetchAllEntities: fetchAllEntities,
+    fetchApplications: fetchApplications,
+    fetchTrainDialogs: fetchTrainDialogs
+  }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(App);
