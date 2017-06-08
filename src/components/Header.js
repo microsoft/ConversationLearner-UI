770791b622b970c5b63979c0b87522ea664d9705
lucashuet93 <lucashh@microsoft.com>
2017-06-07 17:13:16 -0700

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ export default class Header extends React.Component {
                     <span className="ms-font-xxl ms-fontColor-themePrimary"><Link className='headerLink' to="/">BLIS</Link></span>
                 </div>
                 <div className='headerListDiv'>
-                    <span className="ms-font-xxl ms-fontColor-themePrimary"><Link className='headerLink' to="/myApps">MyApps</Link></span>
+                    <span className="ms-font-xxl ms-fontColor-themePrimary"><Link className='headerLink' to="/myApps">My Apps</Link></span>
                     <span className="ms-font-xxl ms-fontColor-themePrimary"><Link className='headerLink' to="/docs">Docs</Link></span>
                     <span className="ms-font-xxl ms-fontColor-themePrimary"><Link className='headerLink' to="/about">About</Link></span>
                     <span className="ms-font-xxl ms-fontColor-themePrimary"><Link className='headerLink' to="/support">Support</Link></span>
