cc758b33b41a4f3069e2ddd0331dc25e11b20047
lucashuet93 <lucashh@microsoft.com>
2017-06-08 13:53:30 -0700

    fetch apps immediately on root comp render

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import '../assets/App.css';
 import BLISAppsHomepage from '../containers/BLISAppsHomepage';
+import { fetchApplications } from '../actions/fetch'
 import Header from './Header';
 import Docs from './Docs';
 import About from './About';
 import Support from './Support';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 class App extends Component {
+  componentWillMount(){
+    this.props.fetchApplications();
+  }
   render() {
     return (
       <Router>
@@ -24,4 +30,9 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        fetchApplications: fetchApplications,
+    }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(App);
