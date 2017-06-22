84e6c9618e65bd1c1f1b9562551cf1cfa67d9d7a
lucashuet93 <lucashh@microsoft.com>
2017-06-22 09:28:58 -0700

    reducer case and state prop for webchat display

--- src/reducers/displayReducer.ts
+++ src/reducers/displayReducer.ts
@@ -1,14 +1,18 @@
 import { ActionObject } from '../actions/ActionObject'
 export interface displayReducerState {
     myAppsDisplay: string,
+    displayWebchat: boolean
 }
 const initialState: displayReducerState = {
     myAppsDisplay: "Home",
+    displayWebchat: false
 };
 export default (state = initialState, action: ActionObject) => {
     switch(action.type) {
         case 'SET_BLIS_APP_DISPLAY':
             return {...state, myAppsDisplay: action.setDisplay};
+        case 'SET_WEBCHAT_DISPLAY':
+            return {...state, displayWebchat: action.setWebchatDisplay};
         default:
             return state;
     }
