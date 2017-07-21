23f87da23d6c5fd08764d8dcb1480e41bd6083c6
lucashuet93 <lucashh@microsoft.com>
2017-07-21 16:05:24 -0700

    chatSessionReducer set up to listen and affect state

--- src/reducers/chatSessionReducer.ts
+++ src/reducers/chatSessionReducer.ts
@@ -1,12 +1,23 @@
-import { ActionObject } from '../types'
+import { ActionObject, ChatSessionState } from '../types'
 import { Reducer } from 'redux'
+import { Session } from 'blis-models'
 
-const initialState: any = {
-	all: []
+const initialState: ChatSessionState = {
+	all: [],
+	current: null
 };
 
-const chatSessionReducer: Reducer<any> =  (state = initialState, action: ActionObject) => {
-    switch(action.type) {        
+const chatSessionReducer: Reducer<ChatSessionState> =  (state = initialState, action: ActionObject) => {
+    switch(action.type) {   
+        case 'FETCH_CHAT_SESSIONS_FULFILLED':
+			return {...state, all: action.allSessions};   
+        case 'CREATE_CHAT_SESSION_FULFILLED':
+            let newSession = {...action.session, sessionID: action.sessionId};
+			return {...state, all: [...state.all, newSession]}  
+        case 'DELETE_CHAT_SESSION_FULFILLED':
+            return state.all.filter((s: Session) => s.sessionId !== action.sessionGUID);
+        case 'SET_CURRENT_CHAT_SESSION':
+			return {...state, current: action.currentSession};
         default:
             return state;
     }
