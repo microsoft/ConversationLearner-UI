a06f2f2294def72db1cecd4ffb837a38ec15ef79
lucashuet93 <lucashh@microsoft.com>
2017-07-28 11:28:50 -0700

    skeleton comps for teach memory, scorer, extractor

---
+++ src/containers/TeachSessionExtractor.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { State } from '../types'
+import { setDisplayMode } from '../actions/updateActions'
+
+class TeachSessionExtractor extends React.Component<any, any> {
+    render() {
+        return (
+            <div>
+                TeachSessionExtractor
+            </div>
+        )
+    }
+}
+const mapDispatchToProps = (dispatch: any) => {
+    return bindActionCreators({
+        setDisplayMode: setDisplayMode
+    }, dispatch);
+}
+const mapStateToProps = (state: State, ownProps: any) => {
+    return {
+        trainDialogs: state.trainDialogs,
+        user: state.user
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(TeachSessionExtractor as React.ComponentClass<any>);
