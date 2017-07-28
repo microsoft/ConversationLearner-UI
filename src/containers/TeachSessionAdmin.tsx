7af5b219de2335000fc570f1883c1c2cf1c0a5ae
Lars Liden <larsliden@gmail.com>
2017-07-28 16:44:53 -0700

    fake user input to extractor

--- src/containers/TeachSessionAdmin.tsx
+++ src/containers/TeachSessionAdmin.tsx
@@ -4,21 +4,40 @@ import { CommandButton } from 'office-ui-fabric-react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { State } from '../types'
+import { UserInput } from 'blis-models'
 import { DisplayMode, TeachMode } from '../types/const';
 import { setDisplayMode } from '../actions/updateActions'
-import { deleteTeachSession } from '../actions/deleteActions'
-import TeachSessionScorer from './TeachSessionScorer'
-import TeachSessionExtractor from './TeachSessionExtractor'
-import TeachSessionMemory from './TeachSessionMemory'
+import { deleteTeachSession } from '../actions/deleteActions';
+import { runExtractor } from '../actions/teachActions';
+import TeachSessionScorer from './TeachSessionScorer';
+import TeachSessionExtractor from './TeachSessionExtractor';
+import TeachSessionMemory from './TeachSessionMemory';
+import { TextFieldPlaceholder } from './TextFieldPlaceholder';
 
 class TeachSessionAdmin extends React.Component<any, any> {
+    constructor(p: any) {
+        super(p);
+        this.state = {
+            userInput: '',
+        };
+    }
     handleAbandon() {
         // TODO: Add confirmation modal
         this.props.setDisplayMode(DisplayMode.AppAdmin);
         let currentAppId: string = this.props.apps.current.appId;
         this.props.deleteTeachSession(this.props.userKey, this.props.teachSession.current, currentAppId);
     }
-
+    nameChanged(text: string) {
+        this.setState({
+            userInput: text
+        })
+    }
+    userInputReceived() {
+        let userInput = new UserInput({text: this.state.userInput});
+        let appId: string = this.props.apps.current.appId;
+        let teachId: string = this.props.teachSession.current;
+        this.props.runExtractor(this.props.user.key, appId, teachId, userInput);
+    }
     render() {
         let userWindow = null;
         switch (this.props.teachSession.mode) {
@@ -41,6 +60,21 @@ class TeachSessionAdmin extends React.Component<any, any> {
                         text='Abandon Teach'
                     />
                 </span>
+                <div>
+                    <TextFieldPlaceholder
+                            onChanged={this.nameChanged.bind(this)}
+                            label="Fake User Input"
+                            placeholder="Name..."
+                            value={this.state.userInput} />
+                    <CommandButton
+                            data-automation-id='randomID13'
+                            className="grayButton"
+                            disabled={false}
+                            onClick={() => this.userInputReceived()}
+                            ariaDescription='Send Text'
+                            text='Send Text'
+                        />
+                </div>
                 <TeachSessionMemory />
                 {userWindow}
             </div>
@@ -50,11 +84,13 @@ class TeachSessionAdmin extends React.Component<any, any> {
 const mapDispatchToProps = (dispatch: any) => {
     return bindActionCreators({
         setDisplayMode: setDisplayMode,
-        deleteTeachSession: deleteTeachSession
+        deleteTeachSession: deleteTeachSession,
+        runExtractor: runExtractor
     }, dispatch);
 }
 const mapStateToProps = (state: State) => {
     return {
+        user: state.user,
         teachSession: state.teachSessions,
         apps: state.apps
     }
