fac6b61ef29a2f069eb61fb8a93589b19c6f5bde
Lars Liden <larsliden@gmail.com>
2017-08-01 11:24:27 -0700

    dummy api data

--- src/containers/TeachSessionExtractor.tsx
+++ src/containers/TeachSessionExtractor.tsx
@@ -2,26 +2,64 @@ import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { State } from '../types'
-import { setDisplayMode } from '../actions/updateActions'
+import { ExtractResponse, TrainExtractorStep } from 'blis-models'
+import { postExtractorFeedback, runScorer } from '../actions/teachActions';
+import { CommandButton } from 'office-ui-fabric-react';
+import { dummyExtractResponse, dummyTrainExtractorStep } from '../epics/apiHelpers' // TEMP
 
 class TeachSessionExtractor extends React.Component<any, any> {
+    sendFeedback() {
+        // TEMP 
+        let trainExtractorStep = dummyTrainExtractorStep();
+        let appId: string = this.props.apps.current.appId;
+        let teachId: string = this.props.teachSession.current.teachId;
+        this.props.postExtractorFeedback(this.props.user.key, appId, teachId, trainExtractorStep);
+    }
+    runScorer() {
+        // TEMP
+        let dummyER = dummyExtractResponse();
+        let extractResponse = dummyER.extractResponse;
+        let appId: string = this.props.apps.current.appId;
+        let teachId: string = this.props.teachSession.current.teachId;
+        this.props.runScorer(this.props.user.key, appId, teachId, extractResponse);
+    }
     render() {
         return (
             <div className="teachSessionHalfMode">
                 TeachSessionExtractor
+
+                <CommandButton
+                        data-automation-id='randomID16'
+                        disabled={false}
+                        onClick={this.sendFeedback.bind(this)}
+                        className='ms-font-su goldButton abandonTeach'
+                        ariaDescription='Send Extract Feedback'
+                        text='Send Extract Feedback'
+                    />
+                <CommandButton
+                        data-automation-id='randomID16'
+                        disabled={false}
+                        onClick={this.runScorer.bind(this)}
+                        className='ms-font-su goldButton abandonTeach'
+                        ariaDescription='Run Scorer'
+                        text='Run Scorer'
+                    />
             </div>
+
         )
     }
 }
 const mapDispatchToProps = (dispatch: any) => {
-    return bindActionCreators({
-        setDisplayMode: setDisplayMode
+    return bindActionCreators({     
+        postExtractorFeedback: postExtractorFeedback,
+        runScorer: runScorer
     }, dispatch);
 }
 const mapStateToProps = (state: State, ownProps: any) => {
     return {
-        trainDialogs: state.trainDialogs,
-        user: state.user
+        user: state.user,
+        teachSession: state.teachSessions,
+        apps: state.apps
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(TeachSessionExtractor as React.ComponentClass<any>);
--- src/containers/TeachSessionScorer.tsx
+++ src/containers/TeachSessionScorer.tsx
@@ -2,26 +2,45 @@ import * as React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { State } from '../types'
-import { setDisplayMode } from '../actions/updateActions'
+import { TrainScorerStep } from 'blis-models';
+import { postScorerFeedback } from '../actions/teachActions'
+import { CommandButton } from 'office-ui-fabric-react';
 
 class TeachSessionScorer extends React.Component<any, any> {
+    sendFeedback() {
+        // TEMP
+        let trainScorerStep = new TrainScorerStep();
+        let appId: string = this.props.apps.current.appId;
+        let teachId: string = this.props.teachSession.current.teachId;
+        this.props.postScorerFeedback(this.props.user.key, appId, teachId, trainScorerStep);
+    }
     render() {
         return (
             <div className="teachSessionHalfMode">
                 TeachSessionScorer
+
+                <CommandButton
+                        data-automation-id='randomID16'
+                        disabled={false}
+                        onClick={this.sendFeedback.bind(this)}
+                        className='ms-font-su goldButton abandonTeach'
+                        ariaDescription='Send Score Feedback'
+                        text='Send Score Feedback'
+                    />
             </div>
         )
     }
 }
 const mapDispatchToProps = (dispatch: any) => {
     return bindActionCreators({
-        setDisplayMode: setDisplayMode
+        postScorerFeedback: postScorerFeedback,
     }, dispatch);
 }
 const mapStateToProps = (state: State, ownProps: any) => {
     return {
-        trainDialogs: state.trainDialogs,
-        user: state.user
+        user: state.user,
+        teachSession: state.teachSessions,
+        apps: state.apps
     }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(TeachSessionScorer as React.ComponentClass<any>);
