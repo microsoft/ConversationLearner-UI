c66d6269d20f683e0a00901ab4771fb51363a816
Matt Mazzola <mattm@microsoft.com>
2017-09-27 17:43:03 -0700

    Change so new epics are implicitly included in the root epic

--- src/epics/root.ts
+++ src/epics/root.ts
@@ -1,47 +1,18 @@
 import 'rxjs';
 import { combineEpics, Epic } from 'redux-observable'
 import { State, ActionObject } from '../types'
-import { fetchApplicationsEpic, fetchBotInfoEpic, fetchEntitiesEpic, fetchActionsEpic, fetchChatSessionsEpic, fetchTeachSessionsEpic, fetchTrainDialogsEpic, fetchLogDialogsEpic } from './fetchEpics'
-import { createNewApplicationEpic, createNewEntityEpic, createNewActionEpic, createNegativeEntity, createNewChatSessionEpic, createNewTeachSessionEpic } from './createEpics'
-import { deleteActionEpic, deleteApplicationEpic, deleteEntityEpic, deleteTrainDialogEpic, deleteLogDialogEpic, deleteReverseEntityEpic, deleteSessionEpic, deleteTeachEpic } from './deleteEpics'
-import { editActionEpic, editApplicationEpic, editEntityEpic, setBlisApplicationEpic } from './updateEpics'
-import { runExtractorEpic, runScorerEpic, scorerFeedbackEpic, postScoreFeedbackFulfilledWaitEpic } from './teachEpics'
+import * as fetchEpics from './fetchEpics'
+import * as createEpics from './createEpics'
+import * as deleteEpics from './deleteEpics'
+import * as updateEpics from './updateEpics'
+import * as teachEpics from './teachEpics'
 
 const rootEpic: Epic<ActionObject, State> = combineEpics(
-	fetchApplicationsEpic,
-	fetchBotInfoEpic,
-	fetchEntitiesEpic,
-	fetchActionsEpic,
-	fetchTrainDialogsEpic,
-	fetchLogDialogsEpic,
-	fetchChatSessionsEpic,
-	fetchTeachSessionsEpic,
-
-	createNewApplicationEpic,
-	createNewEntityEpic,
-	createNewActionEpic,
-	createNewChatSessionEpic,
-	createNegativeEntity,
-	createNewTeachSessionEpic,
-
-	deleteActionEpic,
-	deleteApplicationEpic,
-	deleteEntityEpic,
-	deleteLogDialogEpic,
-	deleteReverseEntityEpic,
-	deleteSessionEpic,
-	deleteTeachEpic,
-	deleteTrainDialogEpic,
-
-	editApplicationEpic,
-	editActionEpic,
-	editEntityEpic,
-
-	setBlisApplicationEpic,
-	runExtractorEpic,
-	runScorerEpic,
-	scorerFeedbackEpic,
-	postScoreFeedbackFulfilledWaitEpic
+	...Object.values(fetchEpics),
+	...Object.values(createEpics),
+	...Object.values(deleteEpics),
+	...Object.values(updateEpics),
+	...Object.values(teachEpics)
 )
 
 export default rootEpic;
