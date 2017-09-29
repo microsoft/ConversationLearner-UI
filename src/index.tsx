4a2aaf37a4d73fcc826c89113d67e66f5d7d346b
Matt Mazzola <mattm@microsoft.com>
2017-09-29 14:23:59 -0700

    Change to use old blis App component and add old css

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createReduxStore } from './reduxStore'
-import App from './components/App';
+// TODO: Use new App which has proper routing
+// import App from './component/App';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+// import './index.css';
 
 ReactDOM.render(
   <Provider store={createReduxStore()}>
