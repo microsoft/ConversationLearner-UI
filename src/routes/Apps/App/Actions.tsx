311838a5e8dca5cfb0900bb2aef7bcc740d87b5c
Matt Mazzola <mattm@microsoft.com>
2017-10-11 13:08:28 -0700

    Clean up Actions page in preparation for new ActionEditor
    - Remove unecessary use of <Link>, <List>, and <EntityTile> components within page
    - Move column render logic to column definition instead splitting it and merging back which is unecessary
    - Add create, and edit actions in prep for new editor which will only give back Actions to create edit or delete, instead of performing the action itself
    (This means all api calls are at a single level of the page, instead of a few being at page level and others being at modal level)

--- src/routes/Apps/App/Actions.tsx
+++ src/routes/Apps/App/Actions.tsx
@@ -2,15 +2,20 @@ import * as React from 'react';
 import { returntypeof } from 'react-redux-typescript';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { createActionAsync } from '../../../actions/createActions'
+import { editActionAsync } from '../../../actions/updateActions'
 import { deleteActionAsync } from '../../../actions/deleteActions'
-import { DetailsList, CommandButton, Link, CheckboxVisibility, List, IColumn, SearchBox } from 'office-ui-fabric-react';
+import { DetailsList, CommandButton, CheckboxVisibility, List, IColumn, SearchBox } from 'office-ui-fabric-react';
 import { BlisAppBase, ActionBase, ModelUtils } from 'blis-models'
 import { ConfirmDeleteModal, ActionResponseCreatorEditor } from '../../../components/modals'
-import EntityTile from '../../../components/EntityTile';
 import { State } from '../../../types'
 import { findDOMNode } from 'react-dom';
 
-let columns: IColumn[] = [
+interface IRenderableColumn extends IColumn {
+    render: (action: ActionBase, component: Actions) => JSX.Element | JSX.Element[]
+}
+
+const columns: IRenderableColumn[] = [
     {
         key: 'payload',
         name: 'Payload',
@@ -18,7 +23,8 @@ let columns: IColumn[] = [
         minWidth: 100,
         maxWidth: 400,
         isResizable: true,
-        isMultiline: true
+        isMultiline: true,
+        render: (action, component) => <span className='ms-font-m-plus' onClick={() => component.onSelectAction(action)}>{ModelUtils.GetPrimaryPayload(action)}</span>
     },
     {
         key: 'arguments',
@@ -26,7 +32,13 @@ let columns: IColumn[] = [
         fieldName: 'arguments',
         minWidth: 80,
         maxWidth: 300,
-        isResizable: true
+        isResizable: true,
+        render: action => {
+            const args = ModelUtils.GetArguments(action);
+            return (!args || args.length === 0)
+                ? <span className="ms-Icon ms-Icon--Remove notFoundIcon" aria-hidden="true"></span>
+                : args.map((argument, i) => <span className='ms-ListItem-primaryText' key={i}>{argument}</span>)
+        }
     },
     {
         key: 'actionType',
@@ -34,7 +46,8 @@ let columns: IColumn[] = [
         fieldName: 'metadata',
         minWidth: 100,
         maxWidth: 100,
-        isResizable: true
+        isResizable: true,
+        render: action => <span className='ms-font-m-plus'>{action.metadata.actionType}</span>
     },
     {
         key: 'requiredEntities',
@@ -42,7 +55,17 @@ let columns: IColumn[] = [
         fieldName: 'requiredEntities',
         minWidth: 100,
         maxWidth: 200,
-        isResizable: true
+        isResizable: true,
+        render: (action, component) => action.requiredEntities.length === 0
+            ? <span className="ms-Icon ms-Icon--Remove blis-icon" aria-hidden="true"></span>
+            : action.requiredEntities.map(entityId => {
+                const entity = component.props.entities.find(e => e.entityId == entityId)
+                return (
+                    <div className='ms-ListItem is-selectable' key={entityId}>
+                        <span className='ms-ListItem-primaryText'>{entity.entityName}</span>
+                    </div>
+                )
+            })
     },
     {
         key: 'negativeEntities',
@@ -50,7 +73,17 @@ let columns: IColumn[] = [
         fieldName: 'negativeEntities',
         minWidth: 100,
         maxWidth: 200,
-        isResizable: true
+        isResizable: true,
+        render: (action, component) => action.negativeEntities.length === 0
+            ? <span className="ms-Icon ms-Icon--Remove blis-icon" aria-hidden="true"></span>
+            : action.negativeEntities.map(entityId => {
+                const entity = component.props.entities.find(e => e.entityId == entityId)
+                return (
+                    <div className='ms-ListItem is-selectable' key={entityId}>
+                        <span className='ms-ListItem-primaryText'>{entity.entityName}</span>
+                    </div>
+                )
+            })
     },
     {
         key: 'suggestedEntity',
@@ -58,7 +91,19 @@ let columns: IColumn[] = [
         fieldName: 'suggestedEntity',
         minWidth: 100,
         maxWidth: 100,
-        isResizable: true
+        isResizable: true,
+        render: action => {
+            const entitySuggestion = (action.metadata as any).entitySuggestion
+            if (!entitySuggestion) {
+                return <span className="ms-Icon ms-Icon--Remove blis-icon" aria-hidden="true"></span>
+            }
+
+            return (
+                <div className='ms-ListItem is-selectable'>
+                    <span className='ms-ListItem-primaryText'>{entitySuggestion.entityName}</span>
+                </div>
+            )
+        }
     },
     {
         key: 'wait',
@@ -66,7 +111,8 @@ let columns: IColumn[] = [
         fieldName: 'isTerminal',
         minWidth: 50,
         maxWidth: 50,
-        isResizable: true
+        isResizable: true,
+        render: action => <span className={"ms-Icon blis-icon " + (action.isTerminal ? 'ms-Icon--CheckMark' : 'ms-Icon--Remove')} aria-hidden="true"></span>
     }
 ];
 
@@ -74,70 +120,71 @@ interface ComponentState {
     actionSelected: ActionBase | null
     actionIDToDelete: string
     columns: IColumn[]
-    confirmDeleteActionModalOpen: boolean
+    isConfirmDeleteActionModalOpen: boolean
     createEditModalOpen: boolean
     searchValue: string
     sortColumn: IColumn
 }
 
-class ActionResponsesHomepage extends React.Component<Props, ComponentState> {
+class Actions extends React.Component<Props, ComponentState> {
     constructor(p: any) {
         super(p);
         this.state = {
             actionIDToDelete: null,
             actionSelected: null,
             searchValue: '',
-            confirmDeleteActionModalOpen: false,
+            isConfirmDeleteActionModalOpen: false,
             createEditModalOpen: false,
             columns: columns,
             sortColumn: null
         }
-        this.deleteSelectedAction = this.deleteSelectedAction.bind(this);
-        this.editSelectedAction = this.editSelectedAction.bind(this)
-        this.renderItemColumn = this.renderItemColumn.bind(this)
-        this.onChange = this.onChange.bind(this)
-        this.onColumnClick = this.onColumnClick.bind(this)
-        this.renderActionItems = this.renderActionItems.bind(this)
-        this.renderEntityList = this.renderEntityList.bind(this)
-        this.handleOpenCreateModal = this.handleOpenCreateModal.bind(this)
-        this.handleCloseCreateModal = this.handleCloseCreateModal.bind(this)
-        this.handleOpenDeleteModal = this.handleOpenDeleteModal.bind(this)
+        this.onClickConfirmDelete = this.onClickConfirmDelete.bind(this);
+        this.onSelectAction = this.onSelectAction.bind(this)
+        this.onChangeSearchString = this.onChangeSearchString.bind(this)
+        this.onClickColumnHeader = this.onClickColumnHeader.bind(this)
+        this.onClickCreateAction = this.onClickCreateAction.bind(this)
+        this.onClickCloseActionEditor = this.onClickCloseActionEditor.bind(this)
+        this.onClickDeleteAction = this.onClickDeleteAction.bind(this)
     }
+
     componentDidMount() {
         this.focusNewActionButton();
     }
-    deleteSelectedAction() {
+
+    focusNewActionButton(): void {
+        findDOMNode<HTMLButtonElement>(this.refs.newAction).focus();
+    }
+
+    onClickConfirmDelete() {
         let actionToDelete = this.props.actions.find(a => a.actionId == this.state.actionIDToDelete)
         this.props.deleteActionAsync(this.props.user.key, this.state.actionIDToDelete, actionToDelete, this.props.app.appId);
         this.setState({
-            confirmDeleteActionModalOpen: false,
+            isConfirmDeleteActionModalOpen: false,
             createEditModalOpen: false,
             actionIDToDelete: null
         })
 
     }
-    focusNewActionButton() : void {
-        findDOMNode<HTMLButtonElement>(this.refs.newAction).focus();
-    }
-    handleCloseDeleteModal() {
+
+    onClickCancelDelete() {
         this.setState({
-            confirmDeleteActionModalOpen: false,
+            isConfirmDeleteActionModalOpen: false,
             actionIDToDelete: null
         })
     }
-    handleOpenDeleteModal(guid: string) {
+    onClickDeleteAction(guid: string) {
         this.setState({
-            confirmDeleteActionModalOpen: true,
+            isConfirmDeleteActionModalOpen: true,
             actionIDToDelete: guid
         })
     }
-    handleOpenCreateModal() {
+    onClickCreateAction() {
         this.setState({
             createEditModalOpen: true,
             actionSelected: null
         })
     }
-    handleCloseCreateModal() {
+    onClickCloseActionEditor() {
         this.setState({
             createEditModalOpen: false,
             actionSelected: null
@@ -146,13 +193,13 @@ class ActionResponsesHomepage extends React.Component<Props, ComponentState> {
             this.focusNewActionButton();
         }, 500);
     }
-    editSelectedAction(action: ActionBase) {
+    onSelectAction(action: ActionBase) {
         this.setState({
             actionSelected: action,
             createEditModalOpen: true
         })
     }
-    onColumnClick(event: any, column: IColumn) {
+    onClickColumnHeader(event: any, column: IColumn) {
         let { columns } = this.state;
         let isSortedDescending = column.isSortedDescending;
 
@@ -175,73 +222,19 @@ class ActionResponsesHomepage extends React.Component<Props, ComponentState> {
             sortColumn: column
         });
     }
-    renderItemColumn(item?: ActionBase, index?: number, column?: IColumn) {
-        let fieldContent = item[column.fieldName];
-        switch (column.key) {
-            case 'wait':
-                if (fieldContent == true) {
-                    return <span className="ms-Icon ms-Icon--CheckMark blis-icon" aria-hidden="true"></span>;
-                } else {
-                    return <span className="ms-Icon ms-Icon--Remove blis-icon" aria-hidden="true"></span>;
-                }
-            case 'actionType':
-                return <span className='ms-font-m-plus'>{fieldContent.actionType}</span>;
-            case 'requiredEntities':
-                if (fieldContent.length > 0) {
-                    return this.renderEntityList(fieldContent)
-                } else {
-                    return <span className="ms-Icon ms-Icon--Remove blis-icon" aria-hidden="true"></span>;
-                }
-            case 'negativeEntities':
-                if (fieldContent.length > 0) {
-                    return this.renderEntityList(fieldContent)
-                } else {
-                    return <span className="ms-Icon ms-Icon--Remove blis-icon" aria-hidden="true"></span>;
-                }
-            case 'suggestedEntity':
-                if (fieldContent != null) {
-                    let entity = this.props.entities.find(e => e.entityId == fieldContent);
-                    let name = entity ? entity.entityName : "!! MISSING ENTITY !!";
-                    return (
-                        <div className='ms-ListItem is-selectable'>
-                            <span className='ms-ListItem-primaryText'>{name}</span>
-                        </div>
-                    )
-                } else {
-                    return <span className="ms-Icon ms-Icon--Remove blis-icon" aria-hidden="true"></span>;
-                }
-            case 'payload':
-                fieldContent = ModelUtils.GetPrimaryPayload(item);
-                return <span className='ms-font-m-plus'><Link onClick={() => this.editSelectedAction(item)}>{fieldContent}</Link></span>;
-            case 'arguments':  
-                let args = ModelUtils.GetArguments(item);
-                if (args)
-                {
-                    return (
-                        <List
-                            items={args}
-                            onRenderCell={(item, index) => (
-                                <span className='ms-ListItem-primaryText'>{item}</span>
-                            )}
-                        />
-                    )
-                }
-                return <span className="ms-Icon ms-Icon--Remove notFoundIcon" aria-hidden="true"></span>;
-            default:
-                return <span className='ms-font-m-plus'>{fieldContent}</span>;
-        }
-    }
-    renderEntityList(entityIDs: string[]) {
-        let entityObjects = entityIDs.map(id => this.props.entities.find(e => e.entityId == id))
+
+    renderEntityList(entityIds: string[]) {
+        const entities = entityIds.map(id => this.props.entities.find(e => e.entityId == id))
         return (
             <List
-                items={entityObjects}
+                items={entities}
                 onRenderCell={(item, index) => (
                     <EntityTile item={item} />
                 )}
             />
         )
     }
+
     getValue(action: ActionBase, col: IColumn): any {
         let value;
         if (col.key == 'actionType') {
@@ -257,7 +250,7 @@ class ActionResponsesHomepage extends React.Component<Props, ComponentState> {
         return value;
     }
 
-    renderActionItems(): ActionBase[] {
+    getFilteredAndSortedActions(): ActionBase[] {
         //runs when user changes the text 
         let lcString = this.state.searchValue.toLowerCase();
         let filteredActions = this.props.actions.filter(a => {
@@ -281,7 +274,7 @@ class ActionResponsesHomepage extends React.Component<Props, ComponentState> {
 
         if (this.state.sortColumn) {
             // Sort the items.
-            filteredActions = filteredActions.concat([]).sort((a: any, b: any) => {
+            filteredActions = [...filteredActions].sort((a, b) => {
                 let firstValue = this.getValue(a, this.state.sortColumn);
                 let secondValue = this.getValue(b, this.state.sortColumn);
 
@@ -296,7 +289,7 @@ class ActionResponsesHomepage extends React.Component<Props, ComponentState> {
 
         return filteredActions;
     }
-    onChange(newValue: string) {
+    onChangeSearchString(newValue: string) {
         //runs when user changes the text 
         let lcString = newValue.toLowerCase();
         this.setState({
@@ -304,47 +297,54 @@ class ActionResponsesHomepage extends React.Component<Props, ComponentState> {
         })
     }
     render() {
-        let actionItems = this.renderActionItems();
+        const actions = this.getFilteredAndSortedActions();
         return (
             <div className="blis-page">
                 <span className="ms-font-xxl">Actions</span>
                 <span className="ms-font-m-plus">Manage a list of actions that your application can take given it's state and user input...</span>
                 <div>
                     <CommandButton
-                        onClick={this.handleOpenCreateModal}
+                        onClick={this.onClickCreateAction}
                         className='blis-button--gold'
                         ariaDescription='Create a New Action'
                         text='New Action'
                         ref='newAction'
                     />
-                    <ActionResponseCreatorEditor
-                        open={this.state.createEditModalOpen}
-                        blisAction={this.state.actionSelected}
-                        handleClose={this.handleCloseCreateModal}
-                        handleOpenDeleteModal={this.handleOpenDeleteModal}
-                    />
                 </div>
                 <SearchBox
                     className="ms-font-m-plus"
-                    onChange={(newValue) => this.onChange(newValue)}
-                    onSearch={(newValue) => this.onChange(newValue)}
+                    onChange={searchString => this.onChangeSearchString(searchString)}
+                    onSearch={searchString => this.onChangeSearchString(searchString)}
                 />
                 <DetailsList
                     className="ms-font-m-plus"
-                    items={actionItems}
+                    items={actions}
                     columns={this.state.columns}
                     checkboxVisibility={CheckboxVisibility.hidden}
-                    onRenderItemColumn={this.renderItemColumn}
-                    onActiveItemChanged={(item) => this.editSelectedAction(item)}
-                    onColumnHeaderClick={this.onColumnClick}
+                    onRenderItemColumn={(action: ActionBase, i, column: IRenderableColumn) => column.render(action, this)}
+                    onActiveItemChanged={action => this.onSelectAction(action)}
+                    onColumnHeaderClick={this.onClickColumnHeader}
                 />
-                <ConfirmDeleteModal open={this.state.confirmDeleteActionModalOpen} onCancel={() => this.handleCloseDeleteModal()} onConfirm={() => this.deleteSelectedAction()} title="Are you sure you want to delete this action?" />
+                <ConfirmDeleteModal
+                    open={this.state.isConfirmDeleteActionModalOpen}
+                    onCancel={() => this.onClickCancelDelete()}
+                    onConfirm={() => this.onClickConfirmDelete()}
+                    title="Are you sure you want to delete this action?"
+                />
+                <ActionResponseCreatorEditor
+                    open={this.state.createEditModalOpen}
+                    blisAction={this.state.actionSelected}
+                    handleClose={this.onClickCloseActionEditor}
+                    handleOpenDeleteModal={this.onClickDeleteAction}
+                />
             </div>
         );
     }
 }
 const mapDispatchToProps = (dispatch: any) => {
     return bindActionCreators({
+        createActionAsync,
+        editActionAsync,
         deleteActionAsync
     }, dispatch)
 }
@@ -365,4 +365,4 @@ const stateProps = returntypeof(mapStateToProps);
 const dispatchProps = returntypeof(mapDispatchToProps);
 type Props = typeof stateProps & typeof dispatchProps & ReceivedProps;
 
-export default connect<typeof stateProps, typeof dispatchProps, ReceivedProps>(mapStateToProps, mapDispatchToProps)(ActionResponsesHomepage);
+export default connect<typeof stateProps, typeof dispatchProps, ReceivedProps>(mapStateToProps, mapDispatchToProps)(Actions);
