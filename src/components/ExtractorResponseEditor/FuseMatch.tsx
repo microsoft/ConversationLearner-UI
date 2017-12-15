6b87d2b207389d794f12c2dd103a9784ce456606
Matt Mazzola <mattm@microsoft.com>
2017-12-15 14:15:13 -0800

    ExtractorResponseEditor cleanup
    - Remove console.log left from debug
    - Add comments for sub components

--- src/components/ExtractorResponseEditor/FuseMatch.tsx
+++ src/components/ExtractorResponseEditor/FuseMatch.tsx
@@ -6,6 +6,9 @@ interface Props {
     matches: MatchedString[]
 }
 
+/**
+ * Display Fuse.io search match with characters from search input having custom style such as highlight or bold
+ */
 export default function FuseMatch({ matches }: Props) {
     return <span>{matches.map((m, i) => <span className={`match-string ${m.matched ? 'match-string--matched' : ''}`} key={i}>{m.text}</span>)}</span>
 }
