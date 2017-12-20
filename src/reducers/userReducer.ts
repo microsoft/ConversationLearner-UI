b13c6ac1f458cad4b50f3555b12bac4bc02b0155
Matt Mazzola <mattm@microsoft.com>
2017-12-20 14:12:56 -0800

    Change user.key to be user.id to match change in BLIS-SDK (#341)
    See: https://github.com/Microsoft/BLIS-SDK/pull/84

--- src/reducers/userReducer.ts
+++ src/reducers/userReducer.ts
@@ -2,7 +2,6 @@ import { ActionObject } from '../types'
 import { UserState } from '../types'
 import { AT } from '../types/ActionTypes'
 import { Reducer } from 'redux'
-import { KeyGen } from 'blis-models'
 
 const initialState: UserState = {
     name: "",
@@ -17,9 +16,13 @@ const userReducer: Reducer<UserState> = (state = initialState, action: ActionObj
             return { ...initialState };
         case AT.SET_USER:
             // TEMP: Until user data is passed through correctly
-            let userdata = { id: action.id, name: action.name };
-            let key = KeyGen.MakeKey(JSON.stringify(userdata));
-            return { ...state, name: action.name, password: action.password, id: action.id, key: key };
+            return {
+                ...state,
+                name: action.name,
+                password: action.password,
+                id: action.id,
+                key: action.id
+            };
         default:
             return state;
     }
