b7898b9961fb9626e1c4cfbadc12f6dfa9e99906
Matt Mazzola <mattm@microsoft.com>
2018-01-18 15:02:06 -0800

    Revert changes to Docs page when testing slate editor (#366)

--- src/routes/Docs.tsx
+++ src/routes/Docs.tsx
@@ -2,66 +2,22 @@ import * as React from 'react'
 import { FormattedMessage } from 'react-intl'
 import { FontClassNames } from 'office-ui-fabric-react'
 import { FM } from '../react-intl-messages'
-import * as ActionPayloadEditor from '../components/modals/ActionPayloadEditor'
 
-interface State {
-    value: ActionPayloadEditor.SlateValue
-}
+const component = () => (
+    <div className="blis-page">
+        <div className={FontClassNames.superLarge}>
+            <FormattedMessage
+                id={FM.DOCS_TITLE}
+                defaultMessage="Docs"
+            />
+        </div>
+        <div className={FontClassNames.mediumPlus}>
+            <FormattedMessage
+                id={FM.PAGE_COMINGSOON}
+                defaultMessage="Coming Soon..."
+            />
+        </div>
+    </div>
+)
 
-const optionsAvailableForPayload: ActionPayloadEditor.IOption[] = [
-    {
-        id: '1',
-        name: 'docs'
-    },
-    {
-        id: '2',
-        name: 'documents'
-    },
-    {
-        id: '3',
-        name: 'doctors'
-    }
-]
-
-export class Docs extends React.Component<{}, State> {
-    state = {
-        value: ActionPayloadEditor.Utilities.createTextValue('initial value')
-    }
-
-    onChangeValue = (value: ActionPayloadEditor.SlateValue) => {
-        this.setState({
-            value
-        })
-    }
-
-    render() {
-        
-        return (
-            <div className="blis-page">
-                <div className={FontClassNames.superLarge}>
-                    <FormattedMessage
-                        id={FM.DOCS_TITLE}
-                        defaultMessage="Docs"
-                    />
-                </div>
-                <div className={FontClassNames.mediumPlus}>
-                    <FormattedMessage
-                        id={FM.PAGE_COMINGSOON}
-                        defaultMessage="Coming Soon..."
-                    />
-                </div>
-                <div>
-                    <ActionPayloadEditor.Editor
-                        options={optionsAvailableForPayload}
-                        value={this.state.value}
-                        placeholder="Phrase..."
-                        onChange={value => this.onChangeValue(value)}
-                        disabled={false}
-                    />
-                </div>
-            </div>
-        )
-    }
-}
-
-export default Docs
+export default component
