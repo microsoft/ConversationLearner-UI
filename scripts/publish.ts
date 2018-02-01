095ecada73a23a50f9254a44c317e91d2c1f9dea
Matt Mazzola <mattm@microsoft.com>
2018-02-01 13:36:02 -0800

    Update script to increment root package version.

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -38,9 +38,25 @@ async function main() {
         const packageJsonObj = await fs.readJson(packageJsonPath)
         const { name, version, description, keywords, author, repository, license } = packageJsonObj
         console.log(`Found name: ${name} version: ${version}`)
+        
+        const [breaking, feature, patch] = (version as string).split('.').map(s => parseInt(s))
+        const nextVersion = `${breaking}.${feature + 1}.${patch}`
+
+        /**
+         * This auto increment and publish is making assumptions that the package we read was the latest package and version published which isn't very robust
+         * This assumes this only runs a master branch and is always the latest commit.  If someones a build for a previous commit this would increment and attempt
+         * to publish for a version which already exists and fail.
+         * 
+         * Alternatives are to query npm; however, semantic-release found the inconsistencies to be challenging and moved to querying git tags.  However, git tags could also
+         * be out of date.
+         */
+        // TODO: Make more robust
+        console.log(`Auto-incrementing minor (feature) version...`)
+        console.log(`Next version: `, nextVersion)
+
         const newPackageJson = {
             name,
-            version,
+            version: nextVersion,
             description,
             keywords,
             author,
@@ -49,6 +65,14 @@ async function main() {
             main: "index.js",
             typings: "index.d.ts"
         }
+
+        const updatedRootPackageJson = {
+            ...packageJsonObj,
+            version: nextVersion
+        }
+
+        console.log(`Writing root package.json with updated version`)
+        await fs.writeJson(packageJsonPath, updatedRootPackageJson, { spaces: '  ' })
 
         const newPackageJsonFilePath = path.join(uiPackagePath, 'package.json')
         console.log(`Writing new package.json to ${newPackageJsonFilePath}`)
