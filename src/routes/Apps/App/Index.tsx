871b881d8830c8b159f32ff8dc979c611c98e905
Matt Mazzola <mattm@microsoft.com>
2018-04-17 14:37:14 -0700

    Fix logic for excluding negative entities to compensate for different object properties from /source vs /entities/id

--- src/routes/Apps/App/Index.tsx
+++ src/routes/Apps/App/Index.tsx
@@ -166,7 +166,7 @@ class Index extends React.Component<Props, ComponentState> {
                                         }</span>
                             </NavLink>
                             <NavLink className="cl-nav-link" to={{ pathname: `${match.url}/entities`, state: { app } }}>
-                                <Icon iconName="List" /><span>Entities</span><span className="count">{this.props.entities.filter(e => typeof e.positiveId === 'undefined').length}</span>
+                                <Icon iconName="List" /><span>Entities</span><span className="count">{this.props.entities.filter(e => typeof e.positiveId === 'undefined' || e.positiveId === null).length}</span>
                             </NavLink>
                             <NavLink className="cl-nav-link" to={{ pathname: `${match.url}/actions`, state: { app } }}>
                                 <Icon iconName="List" /><span>Actions</span><span className="count">{this.props.actions.length}</span>
