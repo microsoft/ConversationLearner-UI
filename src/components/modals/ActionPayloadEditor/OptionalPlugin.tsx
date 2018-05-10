f66cbe1d61436647f27ea76a2218a760520be530
Matt Mazzola <mattm@microsoft.com>
2018-05-10 15:56:28 -0700

    Extend previous optional node around current text if user enters closing character adjacent / outside it
    Fixes VSTS#1328

--- src/components/modals/ActionPayloadEditor/OptionalPlugin.tsx
+++ src/components/modals/ActionPayloadEditor/OptionalPlugin.tsx
@@ -44,14 +44,58 @@ export default function optionalPlugin(inputOptions: Partial<IOptions> = {}) {
                 return true
             }
 
-            if (isWithinOptionalNode && event.key === options.closingCharacter) {
-                event.preventDefault()
-
-                change
+            if (event.key === options.closingCharacter) {
+                if (isWithinOptionalNode) {
+                    event.preventDefault()
+                    
+                    change
                     .insertText(options.closingCharacter)
                     .collapseToStartOfNextText()
+                    
+                    return true
+                }
+                else {
+                    const value = change.value
+                    const previousSibling = value.document.getPreviousSibling(value.selection.startKey)
 
-                return true
+                    if (previousSibling.type !== NodeTypes.Optional) {
+                        return
+                    }
+
+                    const currentNode = value.document.getDescendant(value.selection.startKey)
+                    if (!currentNode) {
+                        return
+                    }
+
+                    const lastTextOfPreviousOptionalNode = previousSibling.getLastText()
+                    if (!lastTextOfPreviousOptionalNode) {
+                        return    
+                    }
+
+                    event.preventDefault()
+                    
+                    // Intention is to extend the optional node to the end of current text
+                    // 1. Remove all text from current node
+                    // 2. If optional node already ends in closing character remove it
+                    // 3. Put text inside the last text node of the optional node
+                    const lastCharacter = lastTextOfPreviousOptionalNode.text[lastTextOfPreviousOptionalNode.text.length - 1]
+                    const insertOffset = lastCharacter === options.closingCharacter
+                        ? lastTextOfPreviousOptionalNode.text.length - 1
+                        : lastTextOfPreviousOptionalNode.text.length
+
+                    change
+                        .removeTextByKey(currentNode.key, 0, currentNode.text.length)
+
+                    if (lastCharacter === options.closingCharacter) {
+                        change
+                            .removeTextByKey(lastTextOfPreviousOptionalNode.key, insertOffset, 1)
+                    }
+
+                    change
+                        .insertTextByKey(lastTextOfPreviousOptionalNode.key, insertOffset, `${currentNode.text}${options.closingCharacter}`)
+
+                    return true
+                }
             }
         },
 
