6d0a237d28d81c6b38abbaa687a1e43868f7f6fb
Minerva Enriquez <v-maenr@microsoft.com>
2018-06-15 12:41:17 -0700

    test: fixing delete model failure

--- cypress/support/components/homepage.js
+++ cypress/support/components/homepage.js
@@ -4,6 +4,7 @@
  */
 
 /** Navigate to the Conversation Learner Homepage */
+
 function navigateTo() {
   cy.server()
   cy.route('GET', '/apps?**').as('getHomePage')
@@ -39,7 +40,7 @@ function createNewModel(modelName) {
 
 /** Delete an existent Model */
 function deleteModel(modelName) {
-  cy.visit('http://localhost:5050/Home')
+  navigateTo()
 
   cy.contains(modelName)
     .parents('.ms-DetailsRow-fields')
@@ -51,4 +52,4 @@ function deleteModel(modelName) {
     .click()
 }
 
-export{navigateTo, createNewModel, deleteModel}
+export{navigateTo, createNewModel, deleteModel }
