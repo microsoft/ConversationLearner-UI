d8fedb538866164eda6201a077045a9f694ddc34
Matt Mazzola <mattm@microsoft.com>
2018-06-27 09:56:52 -0700

    fix: add aria-alert for spinner

--- src/components/modals/SpinnerWindow.tsx
+++ src/components/modals/SpinnerWindow.tsx
@@ -15,17 +15,22 @@ const debug = false;
 class SpinnerWindow extends React.Component<Props, {}> {
     render() {
         return (
-            <Modal
-                isOpen={this.props.displaySpinner.length > 0}
-                isBlocking={true}
-                containerClassName='cl-spinner'>
-                <Spinner size={SpinnerSize.large} />
-                {debug &&
-                    <div>
-                        {this.props.displaySpinner.join("\n\n")}
-                    </div>
-                }
-            </Modal>
+            <React.Fragment>
+                <Modal
+                    isOpen={this.props.displaySpinner.length > 0}
+                    isBlocking={true}
+                    containerClassName='cl-spinner'>
+                    <Spinner size={SpinnerSize.large} />
+                    {debug &&
+                        <div>
+                            {this.props.displaySpinner.join("\n\n")}
+                        </div>
+                    }
+                </Modal>
+                <div role="alert" aria-live="assertive" className="cl-spinner-hidden">
+                    {this.props.displaySpinner.length > 0 && <span>Loading</span>}
+                </div>
+            </React.Fragment>
         );
     }
 }
