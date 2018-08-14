2729c45cc3df5b3b188bc477f1b308ed7fbf1e9a
Matt Mazzola <mattm@microsoft.com>
2018-08-14 16:57:50 -0700

    fix: reset file import on modal open and display error

--- src/components/modals/AppCreator.tsx
+++ src/components/modals/AppCreator.tsx
@@ -88,6 +88,7 @@ class AppCreator extends React.Component<Props, ComponentState> {
             this.setState({
                 appNameVal: '',
                 localeVal: firstValue,
+                file: null
             })
         }
     }
@@ -275,7 +276,7 @@ class AppCreator extends React.Component<Props, ComponentState> {
                             <FilePicker
                                 extensions={['cl']}
                                 onChange={this.onChangeFile}
-                                onError={(error: string) => setErrorDisplay(ErrorType.Error, error, [], null)}
+                                onError={(error: string) => this.props.setErrorDisplay(ErrorType.Error, error, [], null)}
                             >
                                 <div className="cl-action-creator-file-picker">
                                     <OF.PrimaryButton
