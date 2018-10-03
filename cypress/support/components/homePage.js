ce38d28a4eb037c6d370a3c7476f016512abc99a
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2018-10-03 09:53:01 -0700

    backup

--- cypress/support/components/homePage.js
+++ cypress/support/components/homePage.js
@@ -6,5 +6,30 @@
  export function visit()                        { cy.visit('http://localhost:5050') }
  export function navigateToModelPage(modelName) { cy.Get('button.root-65').contains(`${modelName}`).Click() }
 
- 
+ export function createNewModel(modelName) 
+ {
+  // Click the button to create app
+  cy.Get('[data-testid="apps-list-button-create-new"]')
+    .should('be.visible')
+    .Click()
+
+  cy.Get('[data-testid="app-create-input-name"]')
+    .type(modelName)
+
+  cy.Get('[data-testid="app-create-button-submit"]')
+    .Click()
+}
+
+export function deleteModel(modelName) 
+{
+  cy.contains(modelName)
+    .parents('.ms-DetailsRow-fields')
+    .find('i[data-icon-name="Delete"]')
+    .Click()
+
+  cy.Get('.ms-Dialog-main')
+    .contains('Confirm')
+    .Click()
+}
+
 
--- cypress/support/components/logdialogspage.js
+++ cypress/support/components/logdialogspage.js
@@ -2,18 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.  
  * Licensed under the MIT License.
  */
-export function verifyPageTitle() {
-  cy.get('div[data-testid="logdialogs-title"]')
-    .contains('Log Dialogs')
-}
 
-export function createNew() {
-  cy.server()
-  cy.route('PUT', '/sdk/state/conversationId?**').as('putConv')
-
-  cy.get('[data-testid="logdialogs-button-create"]')
-    .click()
-
-  cy.wait('@putConv')
-    .wait(1000)
-}
+export function verifyPageTitle() { cy.Get('div[data-testid="logdialogs-title"]').contains('Log Dialogs') }
+export function createNew() { cy.Get('[data-testid="logdialogs-button-create"]').Click() }
