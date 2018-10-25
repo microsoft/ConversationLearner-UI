f60aeadc19ef26470b712fb18388e4bcb1e13940
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2018-10-25 09:25:43 -0700

    Added some comments explaining how it works

--- cypress/support/TestList.js
+++ cypress/support/TestList.js
@@ -8,8 +8,9 @@ const train = require('../tests/Train')
 const log = require('../tests/Log')
 const helpers = require('./Helpers')
 
-// NOTE: The function name specified by 'func' below must match the 
-//       root of the file name that calls AddToCypressTestList().
+// NOTE: The function name specified by 'func' below must match the root of the
+//       file name that calls AddToCypressTestList(). Also the direct parent
+//       folder of the caller must match the test group name
 var testGroups = 
 [
   { name: 'CreateModels', tests:
@@ -23,7 +24,7 @@ var testGroups =
   [
     {name: "What's Your Name", func: log.WhatsYourName},
   ]},
-  { name: 'train', tests:
+  { name: 'Train', tests:
   [
     {name: "Disqualifying Entities", func: train.DisqualifyingEntities},
     {name: "Wait vs No Wait Action", func: train.WaitVsNoWaitAction},
@@ -32,28 +33,35 @@ var testGroups =
   ]},
 ]
 
+// This function adds test cases to Cypress' test queue. It uses the file path of the calling 
+// function to determin the group name and the test function name. "(All)" is used as a special 
+// name that will cause this method to include all tests for that group to be added to Cypress'
+// test queue. The folder name "Tests", with the file name "(All)" is reserved to be used to 
+// include all groups all test groups to be added to Cypress' test queue.
+//
+// The parenthesis in "(All)" is intended to keep the "All Tests" option at the top of Cypress'
+// test case list since it is produced in alphabetical order.
 export function AddToCypressTestList()
 {
   var funcName = `AddToCypressTestList()`
   var callerFileName = GetCallerFileName()
   
-  // callerFileName looks like: http://localhost:5050/__cypress/tests?p=cypress\integration\tools\zTemp.js-182
+  //                                                                              test group name--\      /--Test function name
+  // callerFileName looks like: http://localhost:5050/__cypress/tests?p=cypress\integration\Tests\Train\WhatsYourName1.js-044
   helpers.ConLog(funcName, `callerFileName: ${callerFileName}`)
 
   var i2 = callerFileName.lastIndexOf('.js-')
   var i1 = callerFileName.lastIndexOf('\\', i2 - 1)
   var testName = callerFileName.substring(i1 + 1, i2)
-  helpers.ConLog(funcName, `testName: ${testName}`)
 
   i2 = i1;
   i1 = callerFileName.lastIndexOf('\\', i2 - 1)
   var testGroupName = callerFileName.substring(i1 + 1, i2)
-  helpers.ConLog(funcName, `testGroup: ${testGroupName}`)
 
   var allTests = (testName == '(All)')
   var allGroups = (allTests && testGroupName == 'Tests')
   var toFind = `function ${testName}(`
-  helpers.ConLog(funcName, `allTests: ${allTests} - allGroups ${allGroups} - toFind: ${toFind}`)
+  helpers.ConLog(funcName, `testGroup: ${testGroupName} - testName: ${testName} - allTests: ${allTests} - allGroups ${allGroups} - toFind: ${toFind}`)
 
   var group
   for (var i = 0; i < testGroups.length; i++)
