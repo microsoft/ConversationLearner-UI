db6a3f98a63f61b0362028bc89350bd2f3219cef
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2018-10-29 11:24:10 -0700

    Update cypress/integration/tools/zTemp.js

--- cypress/integration/tools/zTemp.js
+++ cypress/integration/tools/zTemp.js
@@ -14,42 +14,9 @@ describe('zTemp test', () =>
 {
   it('zTemp test', () => 
   {
-    const newTrainingSummary =
-    {
-      FirstInput: undefined,
-      LastInput: undefined,
-      LastResponse: undefined,
-      Turns: 0,
-      LastModifiedDate: undefined,
-      CreatedDate: undefined,
-    }
-    
-    // Cypress Flaw: to get true global data it must be attached to the window object.
-    window.trainingSummary = newTrainingSummary
-    
-    window.trainingSummary.FirstInput = "1st"
-    window.trainingSummary.LastInput = "last!!!"
-    window.trainingSummary.Turns = "****"
-
-    helpers.ConLog(`######==>`, `FirstInput: ${newTrainingSummary.FirstInput} -- LastInput: ${newTrainingSummary.LastInput} -- LastResponse: ${newTrainingSummary.LastResponse} -- Turns: ${newTrainingSummary.Turns} -- LastModifiedDate: ${newTrainingSummary.LastModifiedDate} -- CreatedDate: ${newTrainingSummary.CreatedDate}`)
-
-    // homePage.Visit()
+    homePage.Visit()
     // homePage.NavigateToModelPage("Model1-mni-Oct26-090220-500")
     // modelPage.NavigateToTrainDialogs()
-
-    // //cy.pause()
-    // cy.WaitForTrainingStatusCompleted()
-
-    // console.log('##2##')
-    // train.CreateNewTrainDialog()
-  
-    // train.TypeYourMessage('Hello')
-  
-    // Cypress.Commands.add("zTemp", () => 
-    // {
-    //   var turns = trainDialogsGrid.GetTurns()
-    //   console.log(`turns.length: ${turns.length} ${turns}`)
-    // })
-    // cy.zTemp()
+    cy.pause()
   })
 })
