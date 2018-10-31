564cb5f71257f197525046118da213248b784e3b
Shahin Shayandeh <shahins@microsoft.com>
2018-10-31 10:54:08 -0700

    fix: editdialogmodal casing

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,7 +15,7 @@ import './components/LogDialogsGrid.js'
 import './components/LogDialogModal.js'
 import './components/ModelPage.js'
 import './components/ScorerModal.js'
-import './components/EditdialogModal.js'
+import './components/EditDialogModal.js'
 import './components/TrainDialogsGrid.js'
 import './Helpers.js'
 
