21470a8fdeb96bfb043e353c8d6ced9b476a84d0
Brian D <pnwbrian@users.noreply.github.com>
2018-11-07 15:05:12 -0800

    Update src/components/modals/ActionScorer.classNames.tsx
    File cleanup

--- src/components/modals/ActionScorer.classNames.tsx
+++
@@ -1,6 +0,0 @@
-// import { /* IStyle */ mergeStyleSets } from '@uifabric/merge-styles';
-import { IButtonStyles } from 'office-ui-fabric-react';
-
-export const getClassNames = (): IButtonStyles => {
-  return { rootDisabled:"background:#ff0000" } };
-};
