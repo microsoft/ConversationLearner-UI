1242767d0b57f7baa96b5b388325154d7182fa05
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2018-11-07 10:12:00 -0800

    fix: minor fixes

--- cypress/integration/tools/zTemp.js
+++ cypress/integration/tools/zTemp.js
@@ -15,38 +15,12 @@ describe('zTemp test', () =>
 {
   it('zTemp test', () => 
   {
-    ArrayTester(['one', 'two', 'three'], ['4444', '55555', '666666'])
-    ArrayTester('one', 'two')
-
-    ArrayTester(undefined, ['4444', '55555', '666666'])
-    ArrayTester(['one', 'two', 'three'])
-    ArrayTester()
-    // homePage.Visit()
+    homePage.Visit()
     // homePage.NavigateToModelPage("BigTrain")
     // modelPage.NavigateToTrainDialogs()
-    // cy.pause()
+    cy.pause()
 
     // editDialogsModal.SelectChatTurn('Hello Paul', 4)
     //cy.Get('div.wc-message-wrapper.list.clickable').contains('Paul is not here').Click()
   })
 })
-
-function ArrayTester(entities1, entities2)
-{ 
-  if (!entities1 && !entities2) helpers.ConLog(`ValidateEntities`, 'IS EMPTY')
-  else
-  {
-    var entities = new Array()
-    if (entities1)
-    {
-      if(!Array.isArray(entities1)) entities1 = [entities1]
-      entities = entities1
-    }
-    if (entities2)
-    {
-      if(!Array.isArray(entities2)) entities2 = [entities2]
-      entities = [...entities, ...entities2]
-    }
-    entities.forEach(entity => { helpers.ConLog(`ValidateEntities`, `entity: "${entity}" -- typeof entity: [${typeof entity}]`)})
-  }
-}
