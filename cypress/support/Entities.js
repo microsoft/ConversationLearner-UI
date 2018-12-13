eccbd222bee9c025a3d46498a2491d0ebe9fa25b
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2018-12-13 12:22:59 -0800

    fix: a better name for this function

--- cypress/support/Entities.js
+++ cypress/support/Entities.js
@@ -18,7 +18,7 @@ export function CreateNewEntity({name, multiValued, negatable, type = 'Custom Tr
   if (negatable) entityModal.ClickNegatableCheckbox()
 
   entityModal.ClickCreateButton()
-  if (type != 'Custom Trained' && type != 'Programmatic') entityModal.ClickOkNoteAboutPreTrainedButton()
+  if (type != 'Custom Trained' && type != 'Programmatic') entityModal.ClickOkButtonOnNoteAboutPreTrained()
 
   if (name) entitiesGrid.VerifyItemInList(name)
   else entitiesGrid.VerifyItemInList(`builtin-${type.toLowerCase()}`)
