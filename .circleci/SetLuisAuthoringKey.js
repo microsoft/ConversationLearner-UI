7b86d7048497b5d477e7dac53efe4b27b15cd4f9
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2018-12-27 15:39:25 -0800

    fix: attempting to create the .env file

--- .circleci/SetLuisAuthoringKey.js
+++ .circleci/SetLuisAuthoringKey.js
@@ -11,30 +11,44 @@ const authoringKeys =
   '55555555555555555555555555555555',
 ]
 
+// Randomly pick a authoring key from array.
+var randomIndex = Math.floor(Math.random() * 5) + 1
+var luisAuthoringKey = authoringKeys[randomIndex]
 
-fs.readFile(envFilePath, (readFileErr, buffer) => 
+// Format the output string
+var dataOut = `LUIS_AUTHORING_KEY=${luisAuthoringKey}\n`
+
+// Rewrite the modified file contents back to the same file.
+fs.writeFile(envFilePath, dataOut, (writeFileErr) => 
 {
-  if (readFileErr) throw readFileErr
-  var dataIn = buffer.toString()
-
-  // Parse the boundaries of the existing authoring key.
-  var indexStart = dataIn.indexOf('LUIS_AUTHORING_KEY=')
-  if (indexStart < 0) throw '"LUIS_AUTHORING_KEY=" not found in .ENV file.'
-  indexStart += 'LUIS_AUTHORING_KEY='.length
-  var indexEnd = dataIn.indexOf('\n', indexStart)
-  
-  // Randomly pick a authoring key from array.
-  var randomIndex = Math.floor(Math.random() * 5) + 1
-  var luisAuthoringKey = authoringKeys[randomIndex]
-
-  // Format the output string
-  var dataOut = dataIn.substring(0, indexStart) + luisAuthoringKey + dataIn.substring(indexEnd)
-  console.log(`The data to write out...\n${dataOut}`);
-
-  // Rewrite the modified file contents back to the same file.
-  fs.writeFile(envFilePath, dataOut, (writeFileErr) => 
-  {
-    if (writeFileErr) throw writeFileErr;
-    console.log('The file has been saved!');
-  })
+  if (writeFileErr) throw writeFileErr;
+  console.log('The .env file has been saved!');
 })
+
+// fs.readFile(envFilePath, (readFileErr, buffer) => 
+// {
+//   if (readFileErr) throw readFileErr
+//   var dataIn = buffer.toString()
+
+//   // Parse the boundaries of the existing authoring key.
+//   var indexStart = dataIn.indexOf('LUIS_AUTHORING_KEY=')
+//   if (indexStart < 0) throw '"LUIS_AUTHORING_KEY=" not found in .ENV file.'
+//   indexStart += 'LUIS_AUTHORING_KEY='.length
+//   var indexEnd = dataIn.indexOf('\n', indexStart)
+  
+//   // Randomly pick a authoring key from array.
+//   var randomIndex = Math.floor(Math.random() * 5) + 1
+//   var luisAuthoringKey = authoringKeys[randomIndex]
+
+//   // Format the output string
+//   var dataOut = dataIn.substring(0, indexStart) + luisAuthoringKey + dataIn.substring(indexEnd)
+//   console.log(`The data to write out...\n${dataOut}`);
+
+//   // Rewrite the modified file contents back to the same file.
+//   fs.writeFile(envFilePath, dataOut, (writeFileErr) => 
+//   {
+//     if (writeFileErr) throw writeFileErr;
+//     console.log('The file has been saved!');
+//   })
+// })
+
