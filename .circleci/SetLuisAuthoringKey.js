89456bcc9cfa5e30a8c4aac136c5ea4c81fe2709
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2018-12-26 20:43:43 -0800

    fix: backup

--- .circleci/SetLuisAuthoringKey.js
+++ .circleci/SetLuisAuthoringKey.js
@@ -1,5 +1,7 @@
 const fs = require('../node_modules/cypress/lib/fs')
 
+const envFilePath = '../cl-samples/.env'
+
 const authoringKeys =
 [
   '11111111111111111111111111111111',
@@ -10,30 +12,30 @@ const authoringKeys =
   '7d7048bb12ec4bc59c988cfa855363de',
 ]
 
-const envFilePath = '../cl-samples/.env'
 
 fs.readFile(envFilePath, (readFileErr, buffer) => 
 {
   if (readFileErr) throw readFileErr
   var dataIn = buffer.toString()
-  
+
+  // Parse the boundaries of the existing authoring key.
   var indexStart = dataIn.indexOf('LUIS_AUTHORING_KEY=')
   if (indexStart < 0) throw '"LUIS_AUTHORING_KEY=" not found in .ENV file.'
   indexStart += 'LUIS_AUTHORING_KEY='.length
   var indexEnd = dataIn.indexOf('\n', indexStart)
   
+  // Randomly pick a authoring key from array.
   var randomIndex = Math.floor(Math.random() * 5) + 1
   var luisAuthoringKey = authoringKeys[randomIndex]
 
+  // Format the output string
   var dataOut = dataIn.substring(0, indexStart) + luisAuthoringKey + dataIn.substring(indexEnd)
   console.log(`The data to write out...\n${dataOut}`);
 
+  // Rewrite the modified file contents back to the same file.
   fs.writeFile(envFilePath, dataOut, (writeFileErr) => 
   {
     if (writeFileErr) throw writeFileErr;
     console.log('The file has been saved!');
   })
 })
-
-
-//node_modules\cypress\lib\fs.js
