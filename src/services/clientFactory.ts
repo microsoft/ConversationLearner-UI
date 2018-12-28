0eab19d38567ec9ce51b3276eb1f600bd1d77710
Lars Liden <larsliden@gmail.com>
2018-12-28 10:31:55 -0800

    feat: bot checksum

--- src/services/clientFactory.ts
+++ src/services/clientFactory.ts
@@ -2,15 +2,18 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.  
  * Licensed under the MIT License.
  */
-import ClClient from './client'
+import ClClient, { ClientHeaders } from './client'
 import { AT } from '../types/ActionTypes'
 import { ErrorInjector } from '../Utils/ErrorInjector';
 //import DebugErrors from '../components/modals/DebugErrors'
 
 let sdkPort = 3978
-let getMemoryKey = (): string => {
-    console.warn(`You attempted to use the Conversation Learner Client before its getMemoryKey method was properly configured. Call setMemoryKey to configure`)
-    return ''
+let getClientHeaders = (): ClientHeaders => {
+    console.warn(`You attempted to use the Conversation Learner Client before its getClientHeaders method was properly configured. Call setClientHeaders to configure`)
+    return {
+        botChecksum: '',
+        memoryKey: ''
+    }
 }
 
 export const getInstance = (actionType: AT): ClClient => {
@@ -18,18 +21,18 @@ export const getInstance = (actionType: AT): ClClient => {
 
     /**
      * Notice we provide wrapping get functions which invoke the local functions
-     * This allows the client to always access the latest of getAccessToken and getMemoryKey without reconstructing a new client
+     * This allows the client to always access the latest of getAccessToken and getClientHeaders without reconstructing a new client
      */
     // TODO: Refactor out the force error argument and need to take in paramter. This should be implemented in another layer as extension not modification
     // TODO: Allow configuration whole URI for SDK to enable communicating with hosted version (Likely change to getter function like access token)
     
-    return new ClClient(`http://localhost:${sdkPort}/sdk`, () => getMemoryKey(), {}, forceError)
+    return new ClClient(`http://localhost:${sdkPort}/sdk`, () => getClientHeaders(), {}, forceError)
 }
 
 export const setPort = (port: number) => {
     sdkPort = port
 }
 
-export const setMemoryKey = (newGetMemoryKey: () => string) => {
-    getMemoryKey = newGetMemoryKey
+export const setHeaders = (newGetClientHeaders: () => ClientHeaders) => {
+    getClientHeaders = newGetClientHeaders
 }
