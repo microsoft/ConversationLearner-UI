61915a994d713e1626ce63a23b67f988eb2b9a01
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-01-10 16:51:56 -0800

    fix: release candidate 1

--- cypress/support/components/HomePage.js
+++ cypress/support/components/HomePage.js
@@ -8,7 +8,7 @@ const settings = require('../components/Settings')
 const helpers = require('../Helpers')
 
 export function Visit()                         { return cy.visit('http://localhost:5050'); VerifyPageTitle() }
-export function VerifyPageTitle()               { cy.Get('[data-testid="model-list-title"]').contains('Create and manage your Conversation Learner models').should('be.visible') }
+export function VerifyPageTitle()               { return cy.Get('[data-testid="model-list-title"]').contains('Create and manage your Conversation Learner models').should('be.visible') }
 export function NavigateToModelPage(name)       { return cy.Get('[data-testid="model-list-model-name"]').ExactMatch(`${name}`).Click() }
 export function ClickNewModelButton()           { return cy.Get('[data-testid="model-list-create-new-button"]').Click() }
 export function ClickImportModelButton()        { return cy.Get('[data-testid="model-list-import-model-button"]').Click() }
@@ -26,46 +26,6 @@ export function GetModelListRowCount()
   .then(gridElement => { var rowCount = +gridElement.attr('aria-rowcount') -1; return rowCount })
 }
 
-// Returns the index to the next potential row to delete, or undefined to indicate complete.
-export function DeleteNextTestGeneratedModel(indexNextPotentialRowToDelete) 
-{
-  var thisFuncName = `DeleteNextTestGeneratedModel`
-  helpers.ConLog(thisFuncName, `Start`)
-
-  return new Promise((resolve) =>
-  {
-    helpers.ConLog(thisFuncName, `Promise has been created and returned.`)
-    cy.Get('[data-testid="model-list-model-name"]').then(elements =>
-    {
-      helpers.ConLog(thisFuncName, `Elements remaining in model list: ${elements.length}`)
-
-      for(var i = indexNextPotentialRowToDelete; i < elements.length; i++) 
-      {
-        var modelName = elements[i].innerText
-        if(modelName.startsWith('z-')) 
-        {
-          helpers.ConLog(thisFuncName, `Found a test model named: "${modelName}"`)
-
-          // NavigateToModelPage(modelName)
-          // modelPage.NavigateToSettings()
-          // settings.DeleteModel(modelName)
-          VerifyPageTitle() // To Ensure we have landed back on this same model list home page.
-          // if(elements.length == 0) cy.DoesNotContain('[data-testid="model-list-model-name"]')
-          // else cy.DoesNotContain('[data-testid="model-list-model-name"]', modelName)
-
-          helpers.ConLog(thisFuncName, `Done deleting model named: "${modelName}"`)
-          cy.Enqueue(() => {helpers.ConLog(thisFuncName, `Enqueued resolve(${i})`); resolve(i)})
-          return
-        }
-      }
-      helpers.ConLog(thisFuncName, `No Models Found!`)
-      cy.Enqueue(() => {helpers.ConLog(thisFuncName, `Enqueued resolve(undefined)`); resolve(undefined)})
-      return
-    })
-    helpers.ConLog(thisFuncName, `regular time frame completed`)
-  })
-}
-
 export function GetModelNameIdList()  
 {
   var listToReturn = new Array()
@@ -74,8 +34,9 @@ export function GetModelNameIdList()
   {
     var modelName = elements[i].innerText
     var modelId = elements[i].getAttribute('data-model-id');
-    listToReturn.push({modelName: modelName, modelId: modelId})
+    listToReturn.push({name: modelName, id: modelId})
     helpers.ConLog('GetModelNameIdList', `modelName: ${modelName} - modelId: ${modelId}`)
   }
+  helpers.ConLog('GetModelNameIdList', `Returning a list of ${listToReturn.length} models to delete`)
   return listToReturn
 }
--- cypress/tests/DeleteAllTestGeneratedModels.js
+++ cypress/tests/DeleteAllTestGeneratedModels.js
@@ -6,91 +6,37 @@
 const homePage = require('../support/components/HomePage')
 const helpers = require('../support/Helpers')
 
-export function DeleteAllTestGeneratedModels() {
+export function DeleteAllTestGeneratedModels() 
+{
   homePage.Visit()
   cy.WaitForStableDOM()
-
   // We must "Enqueue" this function call so that Cypress will have one "Cypress Command" 
   // still running when the DeleteAllRows function exits. If not for this, only one row will
   // get deleted then test execution will stop.
   cy.Enqueue(DeleteAllTestGeneratedModelRows).then(() => { helpers.ConLog(`Delete All Test Generated Models`, `DONE - All test generated models have been Deleted`) })
+  cy.reload()
 }
 
-function xDeleteAllTestGeneratedModelRows() {
-  var indexNextPotentialRowToDelete = 0
-
-  function DeleteATestGeneratedModelRow(resolve) {
-    var thisFuncName = `DeleteATestGeneratedModelRow`
-    helpers.ConLog(thisFuncName, `Trying to find a test generated model to delete...`)
-
-    homePage.DeleteNextTestGeneratedModel(indexNextPotentialRowToDelete).then(indexNextRow => {
-      helpers.ConLog(thisFuncName, `returned from homePage.DeleteNextTestGeneratedModel: ${indexNextRow}`)
-      if (!indexNextRow) {
-        helpers.ConLog(thisFuncName, `DONE - there are no more test generated models to delete`)
-        return resolve()
-      }
-
-      indexNextPotentialRowToDelete = indexNextRow
-      helpers.ConLog(thisFuncName, `nextRow: ${indexNextRow} - nextPotentialRowToDelete: ${indexNextPotentialRowToDelete}`)
-      DeleteATestGeneratedModelRow(resolve)
-    })
-  }
-
-  return new Promise((resolve) => { DeleteATestGeneratedModelRow(resolve) })
-}
-
-function DeleteAllTestGeneratedModelRows() {
+function DeleteAllTestGeneratedModelRows() 
+{
   var thisFuncName = `DeleteAllTestGeneratedModelRows`
   var modelNameIdList = homePage.GetModelNameIdList()
 
-  modelNameIdList.forEach(model => {
-
-    cy.request({ url: `http://localhost:3978/sdk/app/${model.modelId}`, method: "DELETE", headers: { 'x-conversationlearner-memory-key': 'x' } }).then(resp => { cy.ConLog(resp.status); })
-
-  })
-
-  /*
-    for (var i = 0; i < modelNameIdList.length; i++) {
-      helpers.ConLog(thisFuncName, `modelName: ${modelNameIdList[i].modelName} - modelId: ${modelNameIdList[i].modelId}`)
-      if (modelNameIdList[i].modelName.startsWith('z-')) {
-        // var xhttp = new XMLHttpRequest();
-        // xhttp.onreadystatechange = () =>
-        // {
-        //   if (this.readyState == 4 && this.status == 200) {
-        //     todo    document.getElementById("demo").innerHTML = this.responseText;
-        //   }
-        // }
-  
-        // xhttp.open(`DELETE`, `http://localhost:3978/sdk/app/${modelNameIdList[i].modelId}`);
-        // xhttp.setRequestHeader("x-conversationlearner-memory-key", "any value will do");
-        // xhttp.send();
-  
-        cy.request(
-          {
-            url: `http://localhost:3978/sdk/app/${modelNameIdList[i].modelId}`,
-            headers: { 'x-conversationlearner-memory-key': 'x' }
-          }).then((resp) => {
-            expect(resp.status).to.eq(200)
-            helpers.ConLog(thisFuncName, `Delete request completed for modelName: ${modelNameIdList[i].modelName}`)
-          })
-      }
-    }
-  */
-
-}
-
-/*
-function zapAllModelsFromListview()
-{
-  document.querySelectorAll(`[data-testid="model-list-model-name"]`).forEach(node =>
+  modelNameIdList.forEach(modelNameId => 
   {
-    axios({ url:`http://localhost:3978/sdk/app/${node.getAttribute("data-model-id")}`, method:"delete", headers:{ "x-conversationlearner-memory-key":"xx" }})
-  });
-  setTimeout(function() { window.location.reload(), 2000 })
+    if (modelNameId.name.startsWith('z-')) 
+    {
+      helpers.ConLog(thisFuncName, `Sending Request to Delete Model: ${modelNameId.name}`)
+      cy.request(
+      { 
+        url: `http://localhost:3978/sdk/app/${modelNameId.id}`, 
+        method: "DELETE", 
+        headers: { 'x-conversationlearner-memory-key': 'x' } 
+      }).then(resp => 
+      { 
+        helpers.ConLog(thisFuncName, `Response Status: ${resp.status} - Model: ${modelNameId.name}`); 
+        expect(resp.status).to.eq(200)
+      })
+    }
+  })
 }
-  var ax=document.createElement("script");
-  ax.setAttribute("type","text/javascript");
-  ax.setAttribute("src", "https://unpkg.com/axios/dist/axios.min.js");
-  ax.setAttribute("onload", "zapAllModelsFromListview()");
-  document.getElementsByTagName("head")[0].appendChild(ax);
-*/
