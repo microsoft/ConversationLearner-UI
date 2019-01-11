463d57741654496eed09a3f23e466b8d34cc495f
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-01-10 16:59:31 -0800

    fix: after self code review

--- cypress/integration/tools/zTemp.js
+++ cypress/integration/tools/zTemp.js
@@ -15,8 +15,7 @@ describe('zTemp test', () =>
 {
   it('zTemp test', () => 
   {
-    DeleteAllTestGeneratedModels()
-    //homePage.Visit()
+    homePage.Visit()
     // homePage.NavigateToModelPage("BigTrain")
     // modelPage.NavigateToTrainDialogs()
     // cy.pause()
@@ -24,95 +23,3 @@ describe('zTemp test', () =>
   })
 })
 
-export function DeleteAllTestGeneratedModels()
-{
-  homePage.Visit()
-  cy.log('STARTING!!!');
-
-  // We must "Enqueue" this function call so that Cypress will have one "Cypress Command" 
-  // still running when the DeleteAllRows function exits. If not for this, only one row will
-  // get deleted then test execution will stop.
-  cy.Enqueue(DeleteAllTestGeneratedModelRows).then(() => { helpers.ConLog(`Delete All Test Generated Models`, `DONE - All test generated models have been Deleted`) })
-}
-
-function DeleteAllTestGeneratedModelRows()
-{
-  var indexNextPotentialRowToDelete = 0
-
-  function DeleteATestGeneratedModelRow(resolve)
-  {
-    var thisFuncName = `DeleteATestGeneratedModelRow`
-    helpers.ConLog(thisFuncName, `Trying to find a test generated model to delete...`)
-
-    DeleteNextTestGeneratedModel(indexNextPotentialRowToDelete).then(indexNextRow =>
-    {
-      helpers.ConLog(thisFuncName, `returned from homePage.DeleteNextTestGeneratedModel: ${indexNextRow}`)
-      if (!indexNextRow)
-      {
-        helpers.ConLog(thisFuncName, `DONE - there are no more test generated models to delete`)
-        return resolve()
-      }
-      
-      indexNextPotentialRowToDelete = indexNextRow
-      helpers.ConLog(thisFuncName, `nextRow: ${indexNextRow} - nextPotentialRowToDelete: ${indexNextPotentialRowToDelete}`)
-      DeleteATestGeneratedModelRow(resolve)
-    })
-  }
-  
-  return new Promise((resolve) => { DeleteATestGeneratedModelRow(resolve) })
-}
-
-// Returns the index to the next potential row to delete, or undefined to indicate complete.
-function DeleteNextTestGeneratedModel(indexNextPotentialRowToDelete) 
-{
-  var thisFuncName = `DeleteNextTestGeneratedModel`
-  helpers.ConLog(thisFuncName, `Start`)
-
-  return new Promise((resolve) =>
-  {
-    helpers.ConLog(thisFuncName, `Promise has been created and returned.`)
-    cy.Enqueue(()=> { GetNextModelList() }).then(elements =>
-    {
-      helpers.ConLog(thisFuncName, `Elements: ${elements}`)
-
-      if (elements)
-      //for(var i = indexNextPotentialRowToDelete; i < elements.length; i++) 
-      {
-        var modelName = elements[i].innerText
-        if(modelName.startsWith('z-')) 
-        {
-          helpers.ConLog(thisFuncName, `Found a test model named: "${elements}"`)
-
-          // NavigateToModelPage(modelName)
-          // modelPage.NavigateToSettings()
-          // settings.DeleteModel(modelName)
-          //VerifyPageTitle() // To Ensure we have landed back on this same model list home page.
-          // if(elements.length == 0) cy.DoesNotContain('[data-testid="model-list-model-name"]')
-          // else cy.DoesNotContain('[data-testid="model-list-model-name"]', modelName)
-          cy.Enqueue(()=> {helpers.ConLog(thisFuncName, `Doing some cy stuff ${elements}"`)})
-          helpers.ConLog(thisFuncName, `Done deleting model named: "${elements}"`)
-          //cy.Enqueue(() => {helpers.ConLog(thisFuncName, `Enqueued resolve(${i})`); resolve(elements)})
-          return resolve(elements)
-        }
-      }
-      helpers.ConLog(thisFuncName, `No Models Found!`)
-      //cy.Enqueue(() => {helpers.ConLog(thisFuncName, `Enqueued resolve(undefined)`); resolve(undefined)})
-      return resolve(undefined)
-    })
-    helpers.ConLog(thisFuncName, `regular time frame completed`)
-  })
-}
-
-var mlIndex = 0
-function GetNextModelList()
-{
-  return new Promise((resolve) =>
-  {
-    setTimeout(()=>
-    {
-      helpers.ConLog('Delete - GetNextModelList', `mlIndex: ${mlIndex}`)
-      if (mlIndex >= 3) resolve(undefined)
-      resolve(mlIndex++)
-    }, 750)
-  })
-}
