75eb5c2b7739c81a8f70ae95db38ae29479ca683
Brian D <pnwbrian@users.noreply.github.com>
2019-01-14 16:42:19 -0800

    fix: bug 1835 - render session actions with new SessionRenderer

---
+++ src/components/actionPayloadRenderers/SessionPayloadRenderer.tsx
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.  
+ * Licensed under the MIT License.
+ */
+import * as React from 'react'
+import { TextAction, EntityBase, Memory } from '@conversationlearner/models'
+import * as Util from '../../Utils/util'
+import * as OF from 'office-ui-fabric-react'
+
+interface Props {
+    sessionAction: TextAction
+    entities: EntityBase[]
+    // TODO: Find better alternative than null
+    // When memories is null it's assumed parent doesn't have access to it and intends to fallback to the entity names
+    memories: Memory[] | null
+}
+
+export default class Component extends React.Component<Props, {}> {
+
+    render() {
+        const { entities, memories, sessionAction } = this.props
+        const defaultEntityMap = Util.getDefaultEntityMap(entities)
+        const renderStringUsingEntityNames = sessionAction.renderValue(defaultEntityMap, { preserveOptionalNodeWrappingCharacters: true })
+        const renderStringUsingCurrentMemory = memories === null
+            ? null
+            : sessionAction.renderValue(Util.createEntityMapFromMemories(entities, memories), { fallbackToOriginal: true })
+
+        return <div className={`${OF.FontClassNames.mediumPlus} cl-text-payload`}>
+            <div className="cl-text-payload__string" data-testid="action-scorer-text-response">EndSession: {(renderStringUsingCurrentMemory === null)
+                ? `${renderStringUsingEntityNames}`
+                : `${renderStringUsingCurrentMemory}`
+            }
+            </div>
+        </div>
+    }
+}
