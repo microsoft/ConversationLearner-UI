2248ade484594a9e4e1dc8eb2acb4a04b7c1f5e7
Brian D <pnwbrian@users.noreply.github.com>
2019-01-16 10:16:52 -0800

    fix: bug 1853 - tooltip text (revised)
    and some additional corrective punctuation

--- src/components/ToolTips/ToolTips.tsx
+++ src/components/ToolTips/ToolTips.tsx
@@ -300,9 +300,9 @@ export function getTip(tipType: string) {
             return (
                 <div>
                     <h2>Replay May be needed</h2>
-                    <p>One or more edits to Action or Entity properties, may have invalidated turns in this conversation</p>
-                    <p>For example, an Entity that is now multi-value may have only stored one value</p>
-                    <p>This can be resolved by Replaying the dialog, including all API callbacks</p>
+                    <p>One or more edits to Action or Entities may have invalidated inputs in this conversation.</p>
+                    <p>For example, an Entity that is now multi-value may have only stored one value.</p>
+                    <p>This can be resolved by replaying the dialog, including all API callbacks.</p>
 
                     <h4>Before replay:</h4>
                     <img
@@ -326,9 +326,7 @@ export function getTip(tipType: string) {
             return (
                 <div>
                     <h2>Replay is needed</h2>
-                    <p>One or more turns in this conversation may have been invalidated by edits
-                        to Action or Entities. Replay this conversation so it may be used in
-                        training as errors were introduced.</p>
+                    <p>Recommend replaying this conversation as one or more inputs in this conversation may have been invalidated by edits to Action or Entities.</p>
                 </div>
             )
         case TipType.ENTITY_NAME:
