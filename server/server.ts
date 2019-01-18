d7934f88c00243188aa8e3dc9932e6af745cb763
Matt Mazzola <mattm@microsoft.com>
2019-01-18 11:40:35 -0800

    chore: allow Admin interface to be served at path

---
+++ server/server.ts
@@ -0,0 +1,17 @@
+import * as express from 'express'
+import * as path from 'path'
+
+const buildPath = path.join(__dirname, '..', 'build')
+const indexPath = path.join(buildPath, 'index.html')
+
+const router = express.Router({ caseSensitive: false })
+router.use('/ui', express.static(buildPath));
+router.get('/ui/*', function(_, res) {
+    res.sendFile(indexPath)
+})
+
+const app = express()
+app.use(router)
+app.listen(9000, () => {
+    console.log(`Testing ${buildPath} on port 9000`)
+});
