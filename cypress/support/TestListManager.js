a41b738566343789eff48a9fa3b7a027fe823c32
Brian D <pnwbrian@users.noreply.github.com>
2019-01-22 15:54:30 -0800

    Automation end session actions (#947)
    * feat: TEST AUTOMATION: end session action type
    
    * feat: cleaned up test case automation
    
    * test: new end session test
    
    * test: remove duplicate test entry
    
    * test: add endsession test to test manager list
    
    * test: correct user utterance to remove quote
    
    * fix: extend test coverage for adaptive card chats
    
    * fix: support unqiue action,rendering output
    
    * fix: minor tweaks
    
    * fix: rename to endsession

--- cypress/support/TestListManager.js
+++ cypress/support/TestListManager.js
@@ -15,145 +15,144 @@ const helpers = require('./Helpers')
 // This is the list of tests that will be executed when "RunTestsFromList"
 // is selected from the Cypress Test GUI.
 export const testList =
-[
-  "EditAndBranching.WaitNonWaitErrorHandling",
-]
+  [
+    // "EditAndBranching.WaitNonWaitErrorHandling",
+    "EditAndBranching.AddEndSessionAction"
+  ]
 
 // ************ MODIFY THIS LIST *****************************************
 // This is the list of tests that will be executed when "(All)"
 // is selected from the Cypress Test GUI.
 export const regressionTestList =
-[
-  "CreateModels.AllEntityTypes",
-  "CreateModels.DisqualifyingEntities",
-  "CreateModels.WaitVsNoWaitActions",
-  "CreateModels.WhatsYourName",
-  "CreateModels.TagAndFrog",
-  "CreateModels.Travel",
-  "EditAndBranching.VerifyEditTrainingControlsAndLabels",
-  "EditAndBranching.Branching",
-  "EditAndBranching.TagAndFrog",
-  "EditAndBranching.TwoConsecutiveUserInputErrorHandling",
-  "EditAndBranching.WaitNonWaitErrorHandling",
-  "Log.WhatsYourName",
-  "Train.DisqualifyingEntities",
-  "Train.WaitVsNoWaitActions",
-  "Train.WhatsYourName",
-  "Train.MyNameIs",
-  "Train.TagAndFrog",
-//  "Train.BookMeAFlight",
-  "CleanUp.DeleteAllTestGeneratedModels",
-]
+  [
+    "CreateModels.AllEntityTypes",
+    "CreateModels.DisqualifyingEntities",
+    "CreateModels.WaitVsNoWaitActions",
+    "CreateModels.WhatsYourName",
+    "CreateModels.TagAndFrog",
+    "CreateModels.Travel",
+    "EditAndBranching.VerifyEditTrainingControlsAndLabels",
+    "EditAndBranching.Branching",
+    "EditAndBranching.TagAndFrog",
+    "EditAndBranching.TwoConsecutiveUserInputErrorHandling",
+    "EditAndBranching.WaitNonWaitErrorHandling",
+    "Log.WhatsYourName",
+    "Train.DisqualifyingEntities",
+    "Train.WaitVsNoWaitActions",
+    "Train.WhatsYourName",
+    "Train.MyNameIs",
+    "Train.TagAndFrog",
+    //  "Train.BookMeAFlight",
+    "CleanUp.DeleteAllTestGeneratedModels",
+  ]
 
 // Do NOT alter this list except to add in new test cases as they are created.
-export const masterListOfAllTestCases = 
-[
-  "CreateModels.AllEntityTypes",
-  "CreateModels.DisqualifyingEntities",
-  "CreateModels.WaitVsNoWaitActions",
-  "CreateModels.WhatsYourName",
-  "CreateModels.TagAndFrog",
-  "CreateModels.EndlessLoop",
-  "CreateModels.Travel",
-  "EditAndBranching.VerifyEditTrainingControlsAndLabels",
-  "EditAndBranching.Branching",
-  "EditAndBranching.TagAndFrog",
-  "EditAndBranching.TwoConsecutiveUserInputErrorHandling",
-  "EditAndBranching.WaitNonWaitErrorHandling",
-  "Log.WhatsYourName",
-  "Log.EndlessLoop",
-  "Train.DisqualifyingEntities",
-  "Train.WaitVsNoWaitActions",
-  "Train.WhatsYourName",
-  "Train.MyNameIs",
-  "Train.TagAndFrog",
-  "Train.BookMeAFlight",
-  "CleanUp.DeleteAllTestGeneratedModels",
-]
+export const masterListOfAllTestCases =
+  [
+    "CreateModels.AllEntityTypes",
+    "CreateModels.DisqualifyingEntities",
+    "CreateModels.WaitVsNoWaitActions",
+    "CreateModels.WhatsYourName",
+    "CreateModels.TagAndFrog",
+    "CreateModels.EndlessLoop",
+    "CreateModels.Travel",
+    "EditAndBranching.VerifyEditTrainingControlsAndLabels",
+    "EditAndBranching.Branching",
+    "EditAndBranching.TagAndFrog",
+    "EditAndBranching.TwoConsecutiveUserInputErrorHandling",
+    "EditAndBranching.WaitNonWaitErrorHandling",
+    "EditAndBranching.AddEndSessionAction",
+    "Log.WhatsYourName",
+    "Log.EndlessLoop",
+    "Train.DisqualifyingEntities",
+    "Train.WaitVsNoWaitActions",
+    "Train.WhatsYourName",
+    "Train.MyNameIs",
+    "Train.TagAndFrog",
+    "Train.BookMeAFlight",
+    "CleanUp.DeleteAllTestGeneratedModels",
+  ]
 
 // The lists above are in a format that is convenient for a developer to copy
 // and paste in order to create a list of tests.
 //
 // This next list has all the details needed for creating the test for Cypress.
 const testGroups =
-[
-  {
-    name: 'CreateModels', tests:
-    [
-      { name: "All Entity Types", func: createModels.AllEntityTypes },
-      { name: "Disqualifying Entities", func: createModels.DisqualifyingEntities },
-      { name: "Wait vs No Wait Action Tests", func: createModels.WaitVsNoWaitActions },
-      { name: "What's Your Name", func: createModels.WhatsYourName },
-      { name: "Tag and Frog", func: createModels.TagAndFrog },
-      { name: "Endless Loop", func: createModels.EndlessLoop },
-      { name: "Travel", func: createModels.Travel },
-    ]
-  },
-  {
-    name: 'EditAndBranching', tests:
-    [
-      { name: "Verify Edit Training Controls and Labels", func: editAndBranching.VerifyEditTrainingControlsAndLabels },
-      { name: "Branching", func: editAndBranching.Branching },
-      { name: "Tag and Frog", func: editAndBranching.TagAndFrog },
-      { name: "Two Consecutive User Input Error Handling", func: editAndBranching.TwoConsecutiveUserInputErrorHandling },
-      { name: "Wait-Non-Wait Error Handling", func: editAndBranching.WaitNonWaitErrorHandling },
-    ]
-  },
-  {
-    name: 'Log', tests:
-    [
-      { name: "What's Your Name", func: log.WhatsYourName },
-      { name: "Endless Loop", func: log.EndlessLoop },
-    ]
-  },
-  {
-    name: 'Train', tests:
-    [
-      { name: "Disqualifying Entities", func: train.DisqualifyingEntities },
-      { name: "Wait vs No Wait Action", func: train.WaitVsNoWaitActions },
-      { name: "What's Your Name", func: train.WhatsYourName },
-      { name: "My Name Is", func: train.MyNameIs },
-      { name: "Tag and Frog", func: train.TagAndFrog },
-      { name: "Book me a Flight", func: train.BookMeAFlight}
-    ]
-  },
-  {
-    name: 'CleanUp', tests:
-    [
-      { name: 'Delete All Test Generated Models', func: deleteAllTestGeneratedModels.DeleteAllTestGeneratedModels},
-    ]
-  },
-  {
-    name: 'Tools', tests:
-    [
-      { name: 'Visit Home Page', func: tools.VisitHomePage},
-      { name: 'Create Model 1', func: tools.CreateModel1},
-      { name: 'Create Model 2', func: tools.CreateModel2},
-      { name: 'Create Model 3', func: tools.CreateModel3},
-      { name: 'Create Model 4', func: tools.CreateModel4},
-      { name: 'Create Model 5', func: tools.CreateModel5},
-    ]
-  },
-]
+  [
+    {
+      name: 'CreateModels', tests:
+        [
+          { name: "All Entity Types", func: createModels.AllEntityTypes },
+          { name: "Disqualifying Entities", func: createModels.DisqualifyingEntities },
+          { name: "Wait vs No Wait Action Tests", func: createModels.WaitVsNoWaitActions },
+          { name: "What's Your Name", func: createModels.WhatsYourName },
+          { name: "Tag and Frog", func: createModels.TagAndFrog },
+          { name: "Endless Loop", func: createModels.EndlessLoop },
+          { name: "Travel", func: createModels.Travel },
+        ]
+    },
+    {
+      name: 'EditAndBranching', tests:
+        [
+          { name: "Verify Edit Training Controls and Labels", func: editAndBranching.VerifyEditTrainingControlsAndLabels },
+          { name: "Branching", func: editAndBranching.Branching },
+          { name: "Tag and Frog", func: editAndBranching.TagAndFrog },
+          { name: "Two Consecutive User Input Error Handling", func: editAndBranching.TwoConsecutiveUserInputErrorHandling },
+          { name: "Wait-Non-Wait Error Handling", func: editAndBranching.WaitNonWaitErrorHandling },
+          { name: "End-Session-Action", func: editAndBranching.AddEndSessionAction },
+        ]
+    },
+    {
+      name: 'Log', tests:
+        [
+          { name: "What's Your Name", func: log.WhatsYourName },
+          { name: "Endless Loop", func: log.EndlessLoop },
+        ]
+    },
+    {
+      name: 'Train', tests:
+        [
+          { name: "Disqualifying Entities", func: train.DisqualifyingEntities },
+          { name: "Wait vs No Wait Action", func: train.WaitVsNoWaitActions },
+          { name: "What's Your Name", func: train.WhatsYourName },
+          { name: "My Name Is", func: train.MyNameIs },
+          { name: "Tag and Frog", func: train.TagAndFrog },
+          { name: "Book me a Flight", func: train.BookMeAFlight }
+        ]
+    },
+    {
+      name: 'CleanUp', tests:
+        [
+          { name: 'Delete All Test Generated Models', func: deleteAllTestGeneratedModels.DeleteAllTestGeneratedModels },
+        ]
+    },
+    {
+      name: 'Tools', tests:
+        [
+          { name: 'Visit Home Page', func: tools.VisitHomePage },
+          { name: 'Create Model 1', func: tools.CreateModel1 },
+          { name: 'Create Model 2', func: tools.CreateModel2 },
+          { name: 'Create Model 3', func: tools.CreateModel3 },
+          { name: 'Create Model 4', func: tools.CreateModel4 },
+          { name: 'Create Model 5', func: tools.CreateModel5 },
+        ]
+    },
+  ]
 
-export function AddToCypressTestList(testList) 
-{
+export function AddToCypressTestList(testList) {
   var funcName = `AddToCypressTestList()`
   helpers.ConLog(funcName, `List of Tests: ${testList}`)
-  
+
   if (!Array.isArray(testList)) testList = [testList]
-  
+
   var testListIterator = new TestListIterator(testList)
-  
+
   var test = testListIterator.next
-  while (test != undefined)
-  {
+  while (test != undefined) {
     helpers.ConLog(funcName, `Adding Group: ${test.group}`)
-    describe(test.group, () =>
-    {
+    describe(test.group, () => {
       var currentGroupName = test.group
-      while (test != undefined && test.group == currentGroupName)
-      {
+      while (test != undefined && test.group == currentGroupName) {
         helpers.ConLog(funcName, `Adding Test Case: ${test.name}`)
         it(test.name, test.func)
         test = testListIterator.next
@@ -162,13 +161,11 @@ export function AddToCypressTestList(testList)
   }
 }
 
-class TestListIterator
-{
-  constructor(testList)
-  {
+class TestListIterator {
+  constructor(testList) {
     this.testList = testList
     this.index = 0
-    this.currentGroup = {name: ''}
+    this.currentGroup = { name: '' }
   }
 
   // groupName.testName - 'testName' from 'groupName'
@@ -176,8 +173,7 @@ class TestListIterator
   // *.*                - All Groups, All Tests
   // *.testName         - All tests with all groups matching 'testName'
   // groupName.*        - All tests from 'groupName'
-  get next()
-  {
+  get next() {
     if (this.index >= this.testList.length) return undefined
 
     var x = this.testList[this.index].split('.')
@@ -185,39 +181,32 @@ class TestListIterator
     var groupName = x[0]
     var testName = x[1]
 
-    if (this.currentGroup.name != groupName)
-    {
+    if (this.currentGroup.name != groupName) {
       this.currentGroup = GetTestGroup(groupName)
       if (this.currentGroup == undefined) throw `Group '${groupName}' NOT found in testGroups`
     }
-    
+
     var test = GetTest(this.currentGroup, testName)
     if (test == undefined) throw `Test '${testName}' NOT found in test group '${groupName}'`
 
     this.index++
-    return {group: this.currentGroup.name, name: test.name, func: test.func}
+    return { group: this.currentGroup.name, name: test.name, func: test.func }
   }
 }
 
-function GetTestGroup(name)
-{
-  for (var i = 0; i < testGroups.length; i++)
-  {
-    if (testGroups[i].name == name) 
-    {
+function GetTestGroup(name) {
+  for (var i = 0; i < testGroups.length; i++) {
+    if (testGroups[i].name == name) {
       return testGroups[i]
     }
   }
   return undefined
 }
 
-function GetTest(testGroup, testNameToFind)
-{
+function GetTest(testGroup, testNameToFind) {
   var toFind = `function ${testNameToFind}(`
-  for (var i = 0; i < testGroup.tests.length; i++)
-  {
-    if (`${testGroup.tests[i].func}`.startsWith(toFind)) 
-    {
+  for (var i = 0; i < testGroup.tests.length; i++) {
+    if (`${testGroup.tests[i].func}`.startsWith(toFind)) {
       return testGroup.tests[i]
     }
   }
--- cypress/support/Train.js
+++ cypress/support/Train.js
@@ -13,34 +13,30 @@ function Today() { return Cypress.moment().format("MM/DD/YYYY") }
 // Workaround: to get true global data it must be attached to the window object.
 window.currentTrainingSummary = undefined
 
-export function CreateNewTrainDialog()
-{
-  cy.Enqueue(() => 
-  {
+export function CreateNewTrainDialog() {
+  cy.Enqueue(() => {
     var turns = trainDialogsGrid.GetTurns()
-    window.currentTrainingSummary = 
-    {
-      FirstInput: undefined,
-      LastInput: undefined,
-      LastResponse: undefined,
-      Turns: 0,
-      // FUDGING on the time - subtract 10 seconds because the time is set by the server
-      // which is not exactly the same as our test machine.
-      MomentTrainingStarted: Cypress.moment().subtract(10, 'seconds'),  
-      MomentTrainingEnded: undefined,
-      LastModifiedDate: undefined,
-      CreatedDate: undefined,
-      TrainGridRowCount: (turns ? turns.length : 0) + 1
-    }
+    window.currentTrainingSummary =
+      {
+        FirstInput: undefined,
+        LastInput: undefined,
+        LastResponse: undefined,
+        Turns: 0,
+        // FUDGING on the time - subtract 10 seconds because the time is set by the server
+        // which is not exactly the same as our test machine.
+        MomentTrainingStarted: Cypress.moment().subtract(10, 'seconds'),
+        MomentTrainingEnded: undefined,
+        LastModifiedDate: undefined,
+        CreatedDate: undefined,
+        TrainGridRowCount: (turns ? turns.length : 0) + 1
+      }
     window.isBranched = false
   })
   trainDialogsGrid.CreateNewTrainDialog()
 }
 
-export function EditTraining(firstInput, lastInput, lastResponse)
-{
-  cy.Enqueue(() => 
-  {
+export function EditTraining(firstInput, lastInput, lastResponse) {
+  cy.Enqueue(() => {
     var turns = trainDialogsGrid.GetTurns()
     var firstInputs = trainDialogsGrid.GetFirstInputs()
     var lastInputs = trainDialogsGrid.GetLastInputs()
@@ -50,24 +46,22 @@ export function EditTraining(firstInput, lastInput, lastResponse)
 
     helpers.ConLog(`EditTraining(${firstInput}, ${lastInput}, ${lastResponse})`, `${turns.length}, ${lastInputs[0]}, ${lastInputs[1]}, ${lastInputs[2]}`)
 
-    for (var i = 0; i < firstInputs.length; i++)
-    {
-      if (firstInputs[i] == firstInput && lastInputs[i] == lastInput && lastResponses[i] == lastResponse)
-      {
-        window.currentTrainingSummary = 
-        {
-          FirstInput: firstInputs[i],
-          LastInput: lastInputs[i],
-          LastResponse: lastResponses[i],
-          Turns: turns[i],
-          // FUDGING on the time - subtract 10 seconds because the time is set by the server
-          // which is not exactly the same as our test machine.
-          MomentTrainingStarted: Cypress.moment().subtract(10, 'seconds'),  
-          MomentTrainingEnded: undefined,
-          LastModifiedDate: lastModifiedDates[i],
-          CreatedDate: createdDates[i],
-          TrainGridRowCount: (turns ? turns.length : 0)
-        }    
+    for (var i = 0; i < firstInputs.length; i++) {
+      if (firstInputs[i] == firstInput && lastInputs[i] == lastInput && lastResponses[i] == lastResponse) {
+        window.currentTrainingSummary =
+          {
+            FirstInput: firstInputs[i],
+            LastInput: lastInputs[i],
+            LastResponse: lastResponses[i],
+            Turns: turns[i],
+            // FUDGING on the time - subtract 10 seconds because the time is set by the server
+            // which is not exactly the same as our test machine.
+            MomentTrainingStarted: Cypress.moment().subtract(10, 'seconds'),
+            MomentTrainingEnded: undefined,
+            LastModifiedDate: lastModifiedDates[i],
+            CreatedDate: createdDates[i],
+            TrainGridRowCount: (turns ? turns.length : 0)
+          }
         window.originalTrainingSummary = Object.create(window.currentTrainingSummary)
         window.isBranched = false
 
@@ -80,20 +74,16 @@ export function EditTraining(firstInput, lastInput, lastResponse)
   })
 }
 
-export function VerifyErrorsFoundInTraining(firstInput, lastInput, lastResponse)
-{
-  cy.Enqueue(() => 
-  {
+export function VerifyErrorsFoundInTraining(firstInput, lastInput, lastResponse) {
+  cy.Enqueue(() => {
     var firstInputs = trainDialogsGrid.GetFirstInputs()
     var lastInputs = trainDialogsGrid.GetLastInputs()
     var lastResponses = trainDialogsGrid.GetLastResponses()
 
     helpers.ConLog(`VerifyErrorsFoundInTraining(${firstInput}, ${lastInput}, ${lastResponse})`, `Before Loop of ${firstInputs.length}, ${lastInputs[0]}, ${lastInputs[1]}, ${lastInputs[2]}`)
 
-    for (var i = 0; i < firstInputs.length; i++)
-    {
-      if (firstInputs[i] == firstInput && lastInputs[i] == lastInput && lastResponses[i] == lastResponse)
-      {
+    for (var i = 0; i < firstInputs.length; i++) {
+      if (firstInputs[i] == firstInput && lastInputs[i] == lastInput && lastResponses[i] == lastResponse) {
         helpers.ConLog(`VerifyErrorsFoundInTraining(${firstInput}, ${lastInput}, ${lastResponse})`, `Found it at Index: ${i} - ${firstInputs[i]}, ${lastInputs[i]}, ${lastResponses[i]}`)
         trainDialogsGrid.VerifyErrorIconForTrainGridRow(i)
         return
@@ -103,11 +93,9 @@ export function VerifyErrorsFoundInTraining(firstInput, lastInput, lastResponse)
   })
 }
 
-export function TypeYourMessage(message)
-{
+export function TypeYourMessage(message) {
   editDialogModal.TypeYourMessage(message)
-  cy.Enqueue(() => 
-  {
+  cy.Enqueue(() => {
     if (!window.currentTrainingSummary.FirstInput) window.currentTrainingSummary.FirstInput = message
     window.currentTrainingSummary.LastInput = message
     window.currentTrainingSummary.Turns++
@@ -116,46 +104,46 @@ export function TypeYourMessage(message)
 
 // lastResponse parameter is optional. It is necessary only when there are $entities
 // in the Action that produced the Bot's last response.
-export function SelectAction(expectedResponse, lastResponse)
-{
+export function SelectAction(expectedResponse, lastResponse) {
   scorerModal.ClickAction(expectedResponse)
-  cy.Enqueue(() => 
-  { 
+  cy.Enqueue(() => {
     if (lastResponse) window.currentTrainingSummary.LastResponse = lastResponse
     else window.currentTrainingSummary.LastResponse = expectedResponse
   })
 }
 
+export function SelectEndSessionAction(action_selector, expected_rendered, lastResponse) {
+  scorerModal.ClickEndSessionAction(action_selector, expected_rendered)
+  cy.Enqueue(() => {
+    if (lastResponse) window.currentTrainingSummary.LastResponse = lastResponse
+    else window.currentTrainingSummary.LastResponse = expected_rendered
+  })
+}
+
 // This method is used to score AND AUTO-SELECT the action after branching.
-export function ClickScoreActionsButton(lastResponse)
-{
+export function ClickScoreActionsButton(lastResponse) {
   editDialogModal.ClickScoreActionsButton()
-  cy.Enqueue(() => 
-  { 
+  cy.Enqueue(() => {
     window.currentTrainingSummary.LastResponse = lastResponse
   })
 }
 
-export function Save()
-{
+export function Save() {
   editDialogModal.ClickSaveCloseButton()
   trainDialogsGrid.VerifyPageTitle()
-  cy.Enqueue(() => 
-  { 
+  cy.Enqueue(() => {
     // FUDGING on the time - adding 10 seconds because the time is set by the server
     // which is not exactly the same as our test machine.
     window.currentTrainingSummary.MomentTrainingEnded = Cypress.moment().add(10, 'seconds')
-    
+
     if (window.isBranched) VerifyTrainingSummaryIsInGrid(window.originalTrainingSummary)
 
     VerifyTrainingSummaryIsInGrid(window.currentTrainingSummary)
   })
 }
 
-function VerifyTrainingSummaryIsInGrid(trainingSummary)
-{
-  trainDialogsGrid.WaitForGridReadyThen(trainingSummary.TrainGridRowCount, () =>
-  {
+function VerifyTrainingSummaryIsInGrid(trainingSummary) {
+  trainDialogsGrid.WaitForGridReadyThen(trainingSummary.TrainGridRowCount, () => {
     // Keep these lines of logging code in this method, they come in handy when things go bad.
     // helpers.ConLog(`VerifyTrainingSummaryIsInGrid`, `CreatedDate: ${trainingSummary.CreatedDate}`)
     // helpers.ConLog(`VerifyTrainingSummaryIsInGrid`, `LastModifiedDate: ${trainingSummary.LastModifiedDate}`)
@@ -168,76 +156,66 @@ function VerifyTrainingSummaryIsInGrid(trainingSummary)
     var lastResponses = trainDialogsGrid.GetLastResponses()
     var lastModifiedDates = trainDialogsGrid.GetLastModifiedDates()
     var createdDates = trainDialogsGrid.GetCreatedDates()
-    
-    for (var i = 0; i < trainingSummary.TrainGridRowCount; i++)
-    { 
+
+    for (var i = 0; i < trainingSummary.TrainGridRowCount; i++) {
       // helpers.ConLog(`VerifyTrainingSummaryIsInGrid`, `CreatedDates[${i}]: ${createdDates[i]} --- ${helpers.Moment(createdDates[i]).isBetween(trainingSummary.MomentTrainingStarted, trainingSummary.MomentTrainingEnded)}`)
       // helpers.ConLog(`VerifyTrainingSummaryIsInGrid`, `LastModifiedDates[${i}]: ${lastModifiedDates[i]} --- ${helpers.Moment(lastModifiedDates[i]).isBetween(trainingSummary.MomentTrainingStarted, trainingSummary.MomentTrainingEnded)}`)
       // helpers.ConLog(`VerifyTrainingSummaryIsInGrid`, `Turns: ${turns[i]}`)
 
       if (((trainingSummary.LastModifiedDate && lastModifiedDates[i] == trainingSummary.LastModifiedDate) ||
-          helpers.Moment(lastModifiedDates[i]).isBetween(trainingSummary.MomentTrainingStarted, trainingSummary.MomentTrainingEnded)) && 
-          turns[i] == trainingSummary.Turns &&
-          ((trainingSummary.CreatedDate && createdDates[i] == trainingSummary.CreatedDate) ||
-          helpers.Moment(createdDates[i]).isBetween(trainingSummary.MomentTrainingStarted, trainingSummary.MomentTrainingEnded)) && 
-          firstInputs[i] == trainingSummary.FirstInput &&
-          lastInputs[i] == trainingSummary.LastInput &&
-          lastResponses[i] == trainingSummary.LastResponse)
+        helpers.Moment(lastModifiedDates[i]).isBetween(trainingSummary.MomentTrainingStarted, trainingSummary.MomentTrainingEnded)) &&
+        turns[i] == trainingSummary.Turns &&
+        ((trainingSummary.CreatedDate && createdDates[i] == trainingSummary.CreatedDate) ||
+          helpers.Moment(createdDates[i]).isBetween(trainingSummary.MomentTrainingStarted, trainingSummary.MomentTrainingEnded)) &&
+        firstInputs[i] == trainingSummary.FirstInput &&
+        lastInputs[i] == trainingSummary.LastInput &&
+        lastResponses[i] == trainingSummary.LastResponse)
         return
     }
     throw `The grid should, but does not, contain a row with this data in it: FirstInput: ${trainingSummary.FirstInput} -- LastInput: ${trainingSummary.LastInput} -- LastResponse: ${trainingSummary.LastResponse} -- Turns: ${trainingSummary.Turns} -- LastModifiedDate: ${trainingSummary.LastModifiedDate} -- CreatedDate: ${trainingSummary.CreatedDate}`
   })
 }
 
-export function CaptureOriginalChatMessages() 
-{ 
+export function CaptureOriginalChatMessages() {
   cy.WaitForStableDOM().then(() => { window.originalChatMessages = editDialogModal.GetAllChatMessages() })
 }
 
-export function VerifyOriginalChatMessages() 
-{ 
+export function VerifyOriginalChatMessages() {
   VerifyAllChatMessages(() => { return window.originalChatMessages })
 }
 
-export function CaptureEditedChatMessages() 
-{ 
+export function CaptureEditedChatMessages() {
   cy.WaitForStableDOM().then(() => { window.editedChatMessages = editDialogModal.GetAllChatMessages() })
 }
 
-export function VerifyEditedChatMessages() 
-{ 
+export function VerifyEditedChatMessages() {
   VerifyAllChatMessages(() => { return window.editedChatMessages })
 }
 
-function VerifyAllChatMessages(functionGetChatMessagesToBeVerified) 
-{ 
-  cy.WaitForStableDOM().then(() => 
-  {
+function VerifyAllChatMessages(functionGetChatMessagesToBeVerified) {
+  cy.WaitForStableDOM().then(() => {
     var errorMessage = ''
     var chatMessagesToBeVerified = functionGetChatMessagesToBeVerified()
-    var allChatMessages = editDialogModal.GetAllChatMessages()  
+    var allChatMessages = editDialogModal.GetAllChatMessages()
 
-    if (allChatMessages.length != chatMessagesToBeVerified.length) 
+    if (allChatMessages.length != chatMessagesToBeVerified.length)
       errorMessage += `Original chat message count was ${chatMessagesToBeVerified.length}, current chat message count is ${allChatMessages.length}.`
 
     var length = Math.max(allChatMessages.length, chatMessagesToBeVerified.length)
-    for (var i = 0; i < length; i++)
-    {
+    for (var i = 0; i < length; i++) {
       if (i >= allChatMessages.length)
         errorMessage += `-- [${i}] - Original: '${chatMessagesToBeVerified[i]}' is extra'`
       else if (i >= chatMessagesToBeVerified.length)
         errorMessage += `-- [${i}] - Current: '${allChatMessages[i]}' is extra'`
-      else if (allChatMessages[i] != chatMessagesToBeVerified[i]) 
+      else if (allChatMessages[i] != chatMessagesToBeVerified[i])
         errorMessage += `-- [${i}] - Original: '${chatMessagesToBeVerified[i]}' does not match current: '${allChatMessages[i]}'`
     }
     if (errorMessage.length > 0) throw errorMessage
   })
 }
 
-export function BranchChatTurn(originalMessage, newMessage, originalIndex = 0)
-{
-  cy.Enqueue(() =>
-  {
+export function BranchChatTurn(originalMessage, newMessage, originalIndex = 0) {
+  cy.Enqueue(() => {
     originalMessage = originalMessage.replace(/'/g, "’")
 
     editDialogModal.SelectChatTurn(originalMessage, originalIndex)
@@ -247,13 +225,10 @@ export function BranchChatTurn(originalMessage, newMessage, originalIndex = 0)
     // Capture the list of messages currently in the chat, truncate it at the point of branching, then add the new message to it.
     // This array will be used later to validate that the changed chat is persisted.
     var branchedChatMessages
-    cy.WaitForStableDOM().then(() => 
-    { 
+    cy.WaitForStableDOM().then(() => {
       branchedChatMessages = editDialogModal.GetAllChatMessages()
-      for (var i = 0; i < branchedChatMessages.length; i++)
-      {
-        if (branchedChatMessages[i] == originalMessage)
-        {
+      for (var i = 0; i < branchedChatMessages.length; i++) {
+        if (branchedChatMessages[i] == originalMessage) {
           branchedChatMessages.length = i + 1
           branchedChatMessages[i] = newMessage
         }
@@ -262,22 +237,18 @@ export function BranchChatTurn(originalMessage, newMessage, originalIndex = 0)
 
     editDialogModal.BranchChatTurn(newMessage)
     window.isBranched = true
-    window.originalTrainingSummary.TrainGridRowCount ++
-    window.currentTrainingSummary.TrainGridRowCount ++
+    window.originalTrainingSummary.TrainGridRowCount++
+    window.currentTrainingSummary.TrainGridRowCount++
 
     VerifyAllChatMessages(() => { return branchedChatMessages })
   })
 }
 
-export function SelectAndVerifyEachChatTurn(index = 0)
-{
+export function SelectAndVerifyEachChatTurn(index = 0) {
   if (index == 0) editDialogModal.GetAllChatTurns()
-  cy.Get('@allChatTurns').then(elements => 
-  {
-    if (index < elements.length)
-    {
-      cy.wrap(elements[index]).Click().then(() =>
-      {
+  cy.Get('@allChatTurns').then(elements => {
+    if (index < elements.length) {
+      cy.wrap(elements[index]).Click().then(() => {
         editDialogModal.VerifyChatTurnControls(elements[index], index)
         SelectAndVerifyEachChatTurn(index + 1)
       })
@@ -285,8 +256,7 @@ export function SelectAndVerifyEachChatTurn(index = 0)
   })
 }
 
-export function AbandonDialog()
-{
+export function AbandonDialog() {
   editDialogModal.ClickAbandonDeleteButton()
   editDialogModal.ClickConfirmAbandonDialogButton()
 }
