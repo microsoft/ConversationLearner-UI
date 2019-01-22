22547f90dbac7cc031e4b421c6fac2c37f44fd93
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-01-22 11:47:05 -0800

    fix: backup

--- cypress/plugins/FileServices.js
+++ cypress/plugins/FileServices.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url = require('url')  // Needed to do: 'npm install -s url'
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -9,8 +10,11 @@ const server = http.createServer((req, res) => {
   res.end('Hello, World!\n');
 
   var propertyList = ''
-  for(var property in req) propertyList += `${(propertyList.length == 0 ? '' : ', ')}${property}: ${req[property]}`
-  console.log(propertyList)
+  //for(var property in req) propertyList += `${(propertyList.length == 0 ? '' : ', ')}${property}: ${req[property]}`
+  //console.log(req)//propertyList)
+  console.log(req.url)
+  console.log(req.method)
+  console.log(url.parse(req.url))
 });
 
 server.listen(port, hostname, () => {
