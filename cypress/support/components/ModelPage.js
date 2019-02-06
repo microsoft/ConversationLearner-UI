0e355be172e859f052b3aa717bcb2e1c75cdb4f2
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-02-05 18:10:34 -0700

    fix: backup

--- cypress/support/components/ModelPage.js
+++ cypress/support/components/ModelPage.js
@@ -1,3 +1,5 @@
+import { addEmitHelpers } from 'typescript';
+
 /**
  * Copyright (c) Microsoft Corporation. All rights reserved.  
  * Licensed under the MIT License.
@@ -8,6 +10,7 @@ const actionsGrid = require('../../support/components/ActionsGrid')
 const trainDialogsGrid = require('./TrainDialogsGrid')
 const logDialogsGrid = require('../../support/components/LogDialogsGrid')
 const settings = require('../../support/components/Settings')
+const helpers = require('../Helpers');
 
 export function VerifyModelName(name) { cy.Get('[data-testid="app-index-model-name"]').should(el => { expect(el).to.contain(name) }) }
 export function VerifyPageTitle() { cy.Get('[data-testid="dashboard-title"]').contains('Log Dialogs').should('be.visible') }
@@ -34,19 +37,25 @@ export function WaitForTrainingStatusCompleted() {
   if ((currentHtml.includes('data-testid="training-status-polling-stopped-warning"') ||
       currentHtml.includes('data-testid="training-status-failed"')) &&
       (currentTime > canRefreshTrainingStatusTime)) {
-    canRefreshTrainingStatusTime = currentTime + (2 * 1000)
+        
+        // TODO: Remove after we have proof this is working.
+        if (currentHtml.includes('data-testid="training-status-failed"')) {
+          helpers.ConLog('WaitForTrainingStatusCompleted', 'detected data-testid="training-status-failed"');
+        }
 
-    // When we get here it is possible there are two refresh buttons on the page, one that
-    // is covered up by a popup dialog. Unfortunately the .click() function can take only
-    // one element to click on, so this code is an attempt to deal with that issue.
-    //cy.get('[data-testid="training-status-refresh-button"]').then((elements) => { cy.wrap(elements[elements.length - 1]).click() })
-    var elements = Cypress.$('[data-testid="training-status-refresh-button"]');
-    Cypress.$(elements[elements.length - 1]).click();
+        canRefreshTrainingStatusTime = currentTime + (2 * 1000)
 
-    // The reason we need to call this method once again using cy.WaitForTrainingStatusCompleted()
-    // is because the .click() function causes the time out to change to the default of 4 seconds.
-    //cy.WaitForTrainingStatusCompleted()
-    //setTimeout(WaitForTrainingStatusCompleted, 50)
-  }
+        // When we get here it is possible there are two refresh buttons on the page, one that
+        // is covered up by a popup dialog. Unfortunately the .click() function can take only
+        // one element to click on, so this code is an attempt to deal with that issue.
+        //cy.get('[data-testid="training-status-refresh-button"]').then((elements) => { cy.wrap(elements[elements.length - 1]).click() })
+        var elements = Cypress.$('[data-testid="training-status-refresh-button"]');
+        Cypress.$(elements[elements.length - 1]).click();
+
+        // The reason we need to call this method once again using cy.WaitForTrainingStatusCompleted()
+        // is because the .click() function causes the time out to change to the default of 4 seconds.
+        //cy.WaitForTrainingStatusCompleted()
+        //setTimeout(WaitForTrainingStatusCompleted, 50)
+      }
   expect(currentHtml.includes('data-testid="training-status-completed"')).to.equal(true)
 }
