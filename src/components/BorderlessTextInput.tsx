570a0649b105b38608ebf7e9e769442f1a87bb94
Matt Mazzola <mattm@microsoft.com>
2019-02-11 15:36:58 -0800

    fix: simplify BorderlessTextInput
    No need for internal "active" state

--- src/components/BorderlessTextInput.tsx
+++ src/components/BorderlessTextInput.tsx
@@ -1,52 +1,27 @@
 import * as React from 'react'
 import './BorderlessTextInput.css'
 
-interface Props {
+// Renaming from Props because of https://github.com/Microsoft/tslint-microsoft-contrib/issues/339
+interface ReceivedProps {
     id: string
     placeholder: string
     value: string
     onChange: (value: string) => void
 }
 
-interface State {
-    active: boolean
-}
+type Props = ReceivedProps
 
-class component extends React.Component<Props> {
-    state: State = {
-        active: false
-    }
-
-    onFocusInput = () => {
-        this.setState({
-            active: true
-        })
-    }
-
-    onBlurInput = () => {
-        this.setState({
-            active: false
-        })
-    }
-
-    onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.props.onChange(event.target.value)
-    }
-    
-    render () {
-        return (
-            <input className={`cl-borderless-text-input ${this.state.active ? 'cl-borderless-text-input--active' : ''}`}
-                type="text"
-                id={this.props.id}
-                value={this.props.value}
-                placeholder={this.props.placeholder}
-                onChange={this.onChangeInput}
-                onFocus={this.onFocusInput}
-                onBlur={this.onBlurInput}
-                autoComplete="off"
-            />
-        )
-    }
+const component: React.SFC<Props> = (props) => {
+    return (
+        <input className="cl-borderless-text-input"
+            type="text"
+            id={props.id}
+            value={props.value}
+            placeholder={props.placeholder}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => void props.onChange(event.target.value)}
+            autoComplete="off"
+        />
+    )
 }
 
 export default component
