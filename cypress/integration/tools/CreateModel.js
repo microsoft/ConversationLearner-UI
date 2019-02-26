8788567b047cc4e0497252ac38e61cad564b678b
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-02-26 14:25:41 -0800

    fix: tools

--- cypress/integration/tools/CreateModel.js
+++ cypress/integration/tools/CreateModel.js
@@ -1,1 +1,14 @@
-require('../../support/TestListManager').AddToCypressTestList('Tools.CreateModel')
+/**
+* Copyright (c) Microsoft Corporation. All rights reserved.  
+ * Licensed under the MIT License.
+*/
+
+const models = require('../../support/Models')
+const homePage = require('../../support/components/HomePage')
+
+export function CreateModel(name = 'z-model')
+{
+  models.CreateNewModel(name)
+  homePage.Visit()
+  homePage.GetModelListRowCount()
+}
--- cypress/integration/tools/VisitHomePage.js
+++ cypress/integration/tools/VisitHomePage.js
@@ -1,1 +1,12 @@
-require('../../support/TestListManager').AddToCypressTestList('Tools.VisitHomePage')
+/**
+* Copyright (c) Microsoft Corporation. All rights reserved.  
+ * Licensed under the MIT License.
+*/
+
+const homePage = require('../../support/components/HomePage')
+
+export function VisitHomePage()
+{
+  homePage.Visit()
+  homePage.GetModelListRowCount()
+}
