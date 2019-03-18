1ef07976ff5c9d45d3de6e459c8f80a526340617
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-03-18 16:57:07 -0700

    fix: a new approach

--- cypress/integration/zTemp/zTemp.spec.js
+++ cypress/integration/zTemp/zTemp.spec.js
@@ -15,6 +15,21 @@ describe('zTemp', () => {
   it('Temporary Experimental Test', () => {
     let botDomainAndPort = Cypress.env('BOT_DOMAIN_PORT')
     helpers.ConLog('zTemp', botDomainAndPort)
+
+    let dateTimeStrings = []
+    dateTimeStrings.push(Cypress.moment().add(2, 'days').format("MMMDD-HHmmss"))
+    dateTimeStrings.push(Cypress.moment().add(1, 'hours').format("MMMDD-HHmmss"))
+    dateTimeStrings.push(Cypress.moment().subtract(1, 'hours').format("MMMDD-HHmmss"))
+    dateTimeStrings.push(Cypress.moment().subtract(2, 'hours').format("MMMDD-HHmmss"))
+    dateTimeStrings.push(Cypress.moment().subtract(3, 'hours').format("MMMDD-HHmmss"))
+    dateTimeStrings.push(Cypress.moment().subtract(2, 'days').format("MMMDD-HHmmss"))
+
+    let now = Cypress.moment()
+    dateTimeStrings.forEach(dateTimeString => {
+      let dtMoment = Cypress.moment(dateTimeString, "MMMDD-HHmmss")
+      let moreThanTwoHoursAgo = dt.moment.isBefore()
+      helpers.ConLog('zTemp', dtMoment.format('LLL'))
+    })
   })
 
   it.skip('Should NOT allow more than 20 Train Dialog Tags', () => {
