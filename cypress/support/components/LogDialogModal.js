91b5cf88dc94fdcd4f55954e7d494d17cd10bff7
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-03-22 23:25:23 -0700

    fix: dealt with bug and added timing

--- cypress/support/components/LogDialogModal.js
+++ cypress/support/components/LogDialogModal.js
@@ -2,35 +2,49 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.  
  * Licensed under the MIT License.
  */
+import * as helpers from '../Helpers'
 
 export function CreateNewLogDialogButton() { cy.Get('[data-testid="log-dialogs-new-button"]').Click() }
 export function ClickDoneTestingButton() { return cy.Get('[data-testid="chat-session-modal-done-testing-button"]').Click() }
 export function ClickSessionTimeoutButton() { cy.Get('[data-testid="chat-session-modal-session-timeout-button"]').Click() }
 export function TypeYourMessage(message) { cy.Get('input[placeholder="Type your message..."]').type(`${message}{enter}`) }  // data-testid NOT possible
 
+// This function verifies both the input message is reflected back and the response is what we are expecting.
+// This also has the useful side effect of blocking this function from returning until after
+// the response has been returned.
+// If you do not pass in the 'expectedResponse' then it will merely verify that it got a response.
 export function TypeYourMessageValidateResponse(message, expectedResponse) {
-  let originalUtteranceCount = 0
+  let startTime
+  let indexUserMesage
+  let indexBotResponse
+  let expectedUtteranceCount
+
   cy.WaitForStableDOM()
   cy.Enqueue(() => {
     let elements = Cypress.$('.wc-message-content')
-    originalUtteranceCount =  elements.length
+    indexUserMesage = elements.length 
+    indexBotResponse = indexUserMesage + 1
+    expectedUtteranceCount =  indexBotResponse + 1
   })
 
-  cy.Get('input[placeholder="Type your message..."]').type(`${message}{enter}`)  // data-testid NOT possible
+  // data-testid is NOT possible for this field
+  cy.Get('input[placeholder="Type your message..."]').type(`${message}{enter}`).then(() => {
+    startTime = Cypress.moment()
 
-  // Verify both the input message is reflected back and the response is what we are expecting.
-  // This also has the useful side effect of blocking this function from returning until after
-  // the response has been returned.
-  let expectedUtterance = message.replace(/'/g, "’")
-  cy.Get('.wc-message-content').last().contains(expectedUtterance).then(() => {
+    let expectedUtterance = message.replace(/'/g, "’")
     
     // We allow for a long retry timeout because there have been times the Bot is either slow to respond
-    // or it does not respond at all and we want to which of those errors are frequent or rare.
-    cy.get('.wc-message-content', { timeout: 30000 }).should('have.length', originalUtteranceCount + 2).then(elements =>{
-      if (expectedResponse) {
-        expectedUtterance = expectedResponse.replace(/'/g, "’")
-        cy.wrap(elements[elements.length - 1]).contains(expectedUtterance)
-      }
+    // or it does not respond at all and we want to know which of those errors are frequent or rare.
+    cy.get('.wc-message-content', { timeout: 30000 }).should('have.length', expectedUtteranceCount).then(elements =>{
+      let elapsedTime = Cypress.moment().diff(startTime)
+      helpers.ConLog(`TypeYourMessageValidateResponse(${message}, ${expectedResponse})`, `Elapsed Time for Bot's Response: ${elapsedTime}`)
+      
+      cy.wrap(elements[indexUserMesage]).contains(expectedUtterance).then(() => {
+        if (expectedResponse) {
+          expectedUtterance = expectedResponse.replace(/'/g, "’")
+          cy.wrap(elements[indexBotResponse]).contains(expectedUtterance)
+        }
+      })
     })
   })
 }
