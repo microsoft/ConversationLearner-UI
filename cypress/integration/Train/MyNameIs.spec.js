34d054c6966d3fe68d6d2d5c97ed7a2c9ce2821a
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-03-25 11:46:43 -0700

    Fix "Train - My Name Is" test case (#1052)

--- cypress/integration/Train/MyNameIs.spec.js
+++ cypress/integration/Train/MyNameIs.spec.js
@@ -10,38 +10,97 @@ import * as scorerModal from '../../support/components/ScorerModal'
 import * as train from '../../support/Train'
 import * as editDialogModal from '../../support/components/EditDialogModal'
 import * as common from '../../support/Common'
+import * as helpers from '../../support/Helpers'
 
-describe('Train', () => {
-  it('My Name Is', () => {
-    models.ImportModel('z-myNameIs', 'z-myNameIs.cl')
-    modelPage.NavigateToTrainDialogs()
-    cy.WaitForTrainingStatusCompleted()
+describe('My Name Is - Train Dialog', () => {
+  afterEach(helpers.SkipRemainingTestsOfSuiteIfFailed)
 
-    train.CreateNewTrainDialog()
+  context('Setup', () => {
+    it('Should import a model and wait for training to complete', () => {
+      models.ImportModel('z-myNameIs', 'z-myNameIs.cl')
+      modelPage.NavigateToTrainDialogs()
+      cy.WaitForTrainingStatusCompleted()
+    })
+  })
+  
+  context('Train Dialog', () => {
+    it('Should create a new Train Dialog', () => {
+      train.CreateNewTrainDialog()
+    })
 
-    train.TypeYourMessage('My name is David.')
-    editDialogModal.VerifyEntityLabel('David', 'name')
-    editDialogModal.ClickScoreActionsButton()
-    memoryTableComponent.VerifyEntityInMemory('name', ['David'])
-    scorerModal.VerifyContainsDisabledAction(common.whatsYourName)
-    scorerModal.VerifyContainsEnabledAction('Hello David')
-    train.SelectAction('Hello David', 'Hello $name')
+    it('Should auto-label Entity in user utterance based existing Train Dialog', () => {
+      train.TypeYourMessage('My name is David.')
+      editDialogModal.VerifyEntityLabel('David', 'name')
+    })
 
-    cy.WaitForTrainingStatusCompleted()
+    it('Should find labeled Entity in memory', () => {
+      editDialogModal.ClickScoreActionsButton()
+      memoryTableComponent.VerifyEntitiesInMemory('name', ['David'])
+    })
 
-    train.TypeYourMessage('My name is Susan.')
-// TODO: Remove these left indented lines once the product bug is fixed.    
-cy.log('Going to hit bug 1948')
-    editDialogModal.VerifyEntityLabel('Susan', 'name')
-cy.Enqueue(() => {throw new Error('When this test cases gets here, bug 1948 has been fixed...edit the test code and remove this and the above call to cy.log().')})
-    editDialogModal.ClickScoreActionsButton()
-    memoryTableComponent.VerifyEntityInMemory('name', ['Susan'], 'David')
-    scorerModal.VerifyContainsDisabledAction(common.whatsYourName)
-    scorerModal.VerifyContainsEnabledAction('Hello Susan')
-    train.SelectAction('Hello Susan', 'Hello $name')
+    it('Should show one disabled and one enabled Action', () => {
+      scorerModal.VerifyContainsDisabledAction(common.whatsYourName)
+      scorerModal.VerifyContainsEnabledAction('Hello David')
+    })
 
-    train.Save()
+    it('Should be able to select an Action', () => {
+      train.SelectAction('Hello David', 'Hello $name')
+      cy.WaitForTrainingStatusCompleted()
+    })
 
+    it('Should require manual Entity labeling', () => {
+      train.TypeYourMessage('My name is Susan.')
+      editDialogModal.LabelTextAsEntity('Susan', 'name')
+    })
+
+    it('Should find labeled Entity in memory', () => {
+      editDialogModal.ClickScoreActionsButton()
+
+      // TODO: Remove these comments and the 1st 2 lines of code below once this bug has been fixed...
+      // Bug 1956: Replaced Entity Indicator has been lost when training, can still be seen when editing a training
+      // This test will fail once the bug has been fixed.
+      // Also uncomment the 3rd line of code.
+      memoryTableComponent.VerifyNoDisplacedEntityInMemory('David')
+      memoryTableComponent.VerifyEntitiesInMemory('name', ['Susan'])
+      //memoryTableComponent.VerifyEntitiesInMemory('name', ['Susan'], 'David')
+    })
+
+    it('Should show one disabled and one enabled Action', () => {
+      scorerModal.VerifyContainsDisabledAction(common.whatsYourName)
+      scorerModal.VerifyContainsEnabledAction('Hello Susan')
+    })
+
+    it('Should be able to select an Action and save the training', () => {
+      train.SelectAction('Hello Susan', 'Hello $name')
+      train.Save()
+    })
+  })
+  
+  context('Train Dialog Next', () => {
+    it('Should create a new Train Dialog', () => {
+      cy.WaitForTrainingStatusCompleted()
+      train.CreateNewTrainDialog()
+    })
+
+    it('Should auto-label Entity in user utterance based previous Train Dialog', () => {
+      train.TypeYourMessage('My name is Gabriella.')
+      editDialogModal.VerifyEntityLabel('Gabriella', 'name')
+    })
+
+    it('Should find labeled Entity in memory', () => {
+      editDialogModal.ClickScoreActionsButton()
+      memoryTableComponent.VerifyEntitiesInMemory('name', ['Gabriella'])
+    })
+
+    it('Should show one disabled and one enabled Action', () => {
+      scorerModal.VerifyContainsDisabledAction(common.whatsYourName)
+      scorerModal.VerifyContainsEnabledAction('Hello Gabriella')
+    })
+
+    it('Should be able to select an Action and save the training', () => {
+      train.SelectAction('Hello Gabriella', 'Hello $name')
+      train.Save()
+    })
     // Manually EXPORT this to fixtures folder and name it 'z-nameTrained.cl'
   })
 })
