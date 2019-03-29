7bc92eb8ba50f5b4307c43cacf739bfdecf5fa31
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-03-28 20:25:43 -0700

    fix: working code

--- cypress/support/components/ModelPage.js
+++ cypress/support/components/ModelPage.js
@@ -19,10 +19,6 @@ export function NavigateToActions() { cy.Get('[data-testid="app-index-nav-link-a
 export function NavigateToTrainDialogs() { cy.Get('[data-testid="app-index-nav-link-train-dialogs"]').Click(); trainDialogsGrid.VerifyPageTitle() }
 export function NavigateToLogDialogs() { cy.Get('[data-testid="app-index-nav-link-log-dialogs"]').Click(); logDialogsGrid.VerifyPageTitle() }
 export function NavigateToSettings() { cy.Get('[data-testid="app-index-nav-link-settings"]').Click(); settings.VerifyPageTitle() }
-export function VerifyNoErrorIconOnPage() { cy.DoesNotContain('i[data-icon-name="IncidentTriangle"].cl-color-error') }
-
-// For the Left Pane "Train Dialogs" link.
-export function VerifyErrorIconForTrainDialogs() { cy.Get('[data-testid="app-index-nav-link-train-dialogs"]').find('i[data-icon-name="IncidentTriangle"].cl-color-error') }
 
 // To validate that this code works, search src\actions\appActions.ts for these and alter them:
 //   fetchApplicationTrainingStatusThunkAsync
@@ -49,4 +45,46 @@ export function WaitForTrainingStatusCompleted() {
     Cypress.$(elements[elements.length - 1]).click()
   }
   expect(currentHtml.includes('data-testid="training-status-completed"')).to.be.true
+}
+
+export function VerifyNoErrorIconOnPage() { VerifyErrorIcon(true) }
+
+// Verify for just the Left Pane "Train Dialogs" link.
+export function VerifyErrorIconForTrainDialogs() { VerifyErrorIcon(false) }
+
+function VerifyErrorIcon(noErrorIconExpected)
+{
+  let funcName = `VerifyErrorIcon(${noErrorIconExpected})`
+
+  cy.WaitForStableDOM()
+  cy.wrap({ countFound: -1, timesInARowAtThisCount: 0 }, { timeout: 10000 }).should(retryInfo => {
+    const elements = Cypress.$('i[data-icon-name="IncidentTriangle"].cl-color-error')
+    if(elements.length === retryInfo.countFound) { retryInfo.timesInARowAtThisCount ++ }
+    else {
+      // The count changed since the last time we looked at this.
+      retryInfo.countFound = elements.length
+      retryInfo.timesInARowAtThisCount = 0
+    }
+
+    helpers.ConLog(funcName, `Number of Incident Triangles found on page: ${retryInfo.countFound} - Number of times in a row it was found: ${retryInfo.timesInARowAtThisCount}`)
+    if(retryInfo.timesInARowAtThisCount < 15) {
+      throw new Error(`${retryInfo.timesInARowAtThisCount} times in a row we have seen ${retryInfo.countFound} incident triangles on the page, need to see it there 15 times before we can trust it won't change again.`)
+    }
+    
+    // At this point we know that we have seen the same number of incident triangles many times in a row.
+    // Now we need to verify that it is in the state we expect, and if this next part fails, it will retry
+    // up to the time out setting to see if it changes to what we expect.
+
+    if(noErrorIconExpected) {
+      if(elements.length > 0) {
+        throw new Error(`Expected to find no Incident Triangles on the page, yet ${elements.length} were found`)
+      }
+    } else {
+      const parents = Cypress.$(elements).parents('[data-testid="app-index-nav-link-train-dialogs"]')
+      helpers.ConLog(funcName, `Found ${parents.length} parents`)
+      if (parents.length !== 1) {
+        throw new Error('Expected to find an Incident Triangle for Train Dialogs, but did not.')
+      }
+    }
+  })
 }
