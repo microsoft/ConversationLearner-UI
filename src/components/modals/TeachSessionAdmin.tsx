6a9323f2c157f0ab6b9e42ad159907baff20643b
Lars Liden <larsliden@gmail.com>
2019-03-29 12:43:00 -0700

    feat: allow tags and description editing in logs (#1070)

--- src/components/modals/TeachSessionAdmin.tsx
+++ src/components/modals/TeachSessionAdmin.tsx
@@ -305,17 +305,14 @@ class TeachSessionAdmin extends React.Component<Props, ComponentState> {
                         />
                         {isLogDialog ? 'Log Dialog' : 'Train Dialog'}
                     </div>
-                    {isLogDialog
-                        ? <div>{/* placeholder for grid */}</div>
-                        : <DialogMetadata
-                            description={this.props.description}
-                            tags={this.props.tags}
-                            allUniqueTags={this.props.allUniqueTags}
-                            onChangeDescription={this.props.onChangeDescription}
-                            onAddTag={this.props.onAddTag}
-                            onRemoveTag={this.props.onRemoveTag}
-                        />
-                    }
+                    <DialogMetadata
+                        description={this.props.description}
+                        tags={this.props.tags}
+                        allUniqueTags={this.props.allUniqueTags}
+                        onChangeDescription={this.props.onChangeDescription}
+                        onAddTag={this.props.onAddTag}
+                        onRemoveTag={this.props.onRemoveTag}
+                    />
                     <TrainingStatusContainer
                         app={this.props.app}
                     />
