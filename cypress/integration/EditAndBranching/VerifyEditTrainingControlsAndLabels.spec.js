578f533d74e5d0f77bb16d04da119d07c7be16ef
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-04-15 19:34:41 -0700

    fix: backup

--- cypress/integration/EditAndBranching/VerifyEditTrainingControlsAndLabels.spec.js
+++ cypress/integration/EditAndBranching/VerifyEditTrainingControlsAndLabels.spec.js
@@ -7,29 +7,58 @@ import * as models from '../../support/Models'
 import * as modelPage from '../../support/components/ModelPage'
 import * as scorerModal from '../../support/components/ScorerModal'
 import * as train from '../../support/Train'
+import * as helpers from '../../support/Helpers'
 
-describe('EditAndBranching', () => {
-  it('Verify Edit Training Controls And Labels', () => {
-    models.ImportModel('z-editContols', 'z-nameTrained.cl')
-    modelPage.NavigateToTrainDialogs()
+describe('Verify Edit Training Controls And Labels - EditAndBranching', () => {
+  afterEach(helpers.SkipRemainingTestsOfSuiteIfFailed)
 
-    train.EditTraining('My name is David.', 'My name is Susan.', 'Hello $name')
-    train.CaptureOriginalChatMessages()
+  context('Setup', () => {
+    it('Should import a model to test against and navigate to the Train Dialogs', () => {
+      models.ImportModel('z-editContols', 'z-nameTrained.cl')
+      modelPage.NavigateToTrainDialogs()
+    })
+  })
 
-    train.VerifyCloseButtonLabel()
-    train.VerifyDeleteButtonLabel()
+  context('Edit Train Dialog', () => {
+    it('Should edit a Train Dialog and capture the chat messages to verifiy later', () => {
+      train.EditTraining('My name is David.', 'My name is Susan.', 'Hello $name')
+      train.CaptureOriginalChatMessages()
+    })
 
-    train.VerifyThereAreNoChatEditControls('My name is David.', 'Hello Susan')
-    train.SelectAndVerifyEachChatTurn()
+    it('Should verify the labels for the Close and Delete buttons', () => {
+      train.VerifyCloseButtonLabel()
+      train.VerifyDeleteButtonLabel()
+    })
 
-    train.BranchChatTurn('My name is Susan.', 'I am Groot')
-    train.VerifySaveBranchButtonLabel()
-    train.VerifyAbandonBranchButtonLabel()
+    it('Should verify there are no edit controls visible in the chat pane', () => {
+      train.VerifyThereAreNoChatEditControls('My name is David.', 'Hello Susan')
+    })
 
-    train.VerifyThereAreNoChatEditControls('I am Groot', 'Hello David')
-    train.AbandonBranchChanges()
+    it('Should verify each chat turn contains only the expected buttons based on position in the chat and User or Bot turn', () => {
+      train.SelectAndVerifyEachChatTurn()
+    })
 
-    train.EditTraining('My name is David.', 'My name is Susan.', 'Hello $name')
-    train.VerifyOriginalChatMessages()
+    it('Should branch the Train Dialog at a specific chat turn', () => {
+      train.BranchChatTurn('My name is Susan.', 'I am Groot')
+    })
+
+    it('Should verify that labels changed on two of the buttons to "Save Branch" and "Abandon Branch"', () => {
+      train.VerifySaveBranchButtonLabel()
+      train.VerifyAbandonBranchButtonLabel()
+    })
+
+    it('Should verify there are no edit controls visible in the chat pane', () => {
+      train.VerifyThereAreNoChatEditControls('I am Groot', 'Hello David')
+    })
+
+    it('Should abandon the branched training and verify the original training remains in its original state', () => {
+      train.AbandonBranchChanges()
+      train.EditTraining('My name is David.', 'My name is Susan.', 'Hello $name')
+      train.VerifyOriginalChatMessages()
+    })
+
+    it('Should close the Train Dialog', () => {
+      train.ClickSaveCloseButton()
+    })
   })
 })
