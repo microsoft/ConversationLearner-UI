3c8279fec44c3e529395e936cab8d4f52faaad89
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-04-20 21:28:47 -0700

    fix: the forgotton one

--- cypress/support/components/MergeModal.js
+++ cypress/support/components/MergeModal.js
@@ -5,9 +5,10 @@
 
 import * as helpers from '../../support/Helpers'
 
-export function ClickRefreshScoreButton() { cy.Get('[data-testid="teach-session-admin-refresh-score-button"]').Click() }
-export function SelectAnAction() { cy.Get('[data-testid="action-scorer-button-clickable"]').should("be.visible").Click() }
-export function ClickAddActionButton() { cy.Get('[data-testid="action-scorer-add-action-button"]').Click() }
-export function VerifyMissingActionNotice() { cy.Get('.cl-font--warning').ExactMatch('MISSING ACTION') }
+export function VerifyPageTitle() { cy.Get('[data-testid="merge-modal-title"]').contains('Train Dialogs').should('be.visible') }
 
 //export function 
+//data-testid="merge-modal-title"
+//data-testid="traindialog-title"
+//data-testid="train-dialogs-title"
+
