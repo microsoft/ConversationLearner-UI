859c57db8664ec3c66ee959c2f65b8769a8afd28
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-04-22 16:57:23 -0700

    fix: ready for pr

--- .circleci/SetLuisAuthoringKey.js
+++ .circleci/SetLuisAuthoringKey.js
@@ -11,11 +11,15 @@ let circleCiBuildNumber = +process.env.CIRCLE_BUILD_NUM
 
 // We have 5 LUIS Authoring Keys that we rotate through.
 // We use the Circle CI Build Number to help us get an index to each in sequence.
-// Each PUSH into 
-let akIndex = Math.floor((circleCiBuildNumber % 10) / 2)
+// A Build + Test Run is 2 Workflows, each having its own Build Number.
+// The Build part does NOT consume a LUIS Authoring Keys, which affects our algorithm,
+// thus we need to use one LUIS Authoring Key for every other Build Number.
+//
+// While this is not perfect, it does work most of the time, and using the same
+// key twice in a row everyonce in a while should work out just fine.
+let authoringKeyIndex = Math.floor((circleCiBuildNumber % 10) / 2)
 
-
-let luisAuthoringKey = authoringKeys[akIndex]
+let luisAuthoringKey = authoringKeys[authoringKeyIndex]
 
 console.log(`export LUIS_AUTHORING_KEY=${luisAuthoringKey}\n`)
 
