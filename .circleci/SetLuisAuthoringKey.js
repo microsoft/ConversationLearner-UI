400759b1858a826024d7c94a413a8fb32c334258
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-04-22 16:22:59 -0700

    fix: the working code

--- .circleci/SetLuisAuthoringKey.js
+++ .circleci/SetLuisAuthoringKey.js
@@ -7,12 +7,15 @@ const authoringKeys =
   process.env.LUIS_AUTHORING_KEY_ALT_5,
 ]
 
-// Randomly pick an authoring key from the array.
-//let randomIndex = new Date().getTime() % 5
-
 let circleCiBuildNumber = +process.env.CIRCLE_BUILD_NUM
-let randomIndex = Math.floor((circleCiBuildNumber % 10) / 2)
-let luisAuthoringKey = authoringKeys[randomIndex]
+
+// We have 5 LUIS Authoring Keys that we rotate through.
+// We use the Circle CI Build Number to help us get an index to each in sequence.
+// Each PUSH into 
+let akIndex = Math.floor((circleCiBuildNumber % 10) / 2)
+
+
+let luisAuthoringKey = authoringKeys[akIndex]
 
 console.log(`export LUIS_AUTHORING_KEY=${luisAuthoringKey}\n`)
 
