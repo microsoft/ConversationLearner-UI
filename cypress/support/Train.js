30b8c7c79e73df0b2b3b5912ab02574954a02e19
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-04-24 12:00:31 -0700

    fix: backup

--- cypress/support/Train.js
+++ cypress/support/Train.js
@@ -196,6 +196,8 @@ export function VerifyCyDoesNotContainMethodWorksWithSpecialChatSelector(userMes
 }
 
 export function LabelTextAsEntity(text, entity, itMustNotBeLabeledYet = true) {
+  const funcName = `LabelTextAsEntity(${text}, ${entity}, ${itMustNotBeLabeledYet})`
+
   function LabelIt() {
     // This actually works if text is a word or a phrase.
     cy.Get('body').trigger('Test_SelectWord', { detail: text })
@@ -210,9 +212,26 @@ export function LabelTextAsEntity(text, entity, itMustNotBeLabeledYet = true) {
       let found = false
       const elements = Cypress.$('[data-testid="token-node-entity-value"] > span > span')
 
+helpers.ConLog(funcName, `elements.length=${elements.length}`)
+for (let i = 0; i < elements.length; i++) helpers.Dump(funcName, elements[i])
+helpers.ConLog(funcName, `Now lets try elements.find...`)
+
       // If you need to find a phrase, this part of the code will fail, 
       // you will need to upgrade this code in that case.
-      const element = elements.find(element => helpers.TextContentWithoutNewlines(element) === text)
+      let element
+      for (let i = 0; i < elements.length; i++) {
+helpers.ConLog(funcName, elements[i].textContent)
+        if (helpers.TextContentWithoutNewlines(elements[i]) === text) {
+          element = elements[i]
+          break;
+        }
+      }
+      
+//       const element = elements.find(element => {
+// helpers.ConLog(funcName, element.textContent)
+//         return helpers.TextContentWithoutNewlines(element) === text
+//       })
+
       if (element) {
         found = Cypress.$(element).parents('.cl-entity-node--custom').find(`[data-testid="custom-entity-name-button"]:contains('${entity}')`).length == 0
       }
