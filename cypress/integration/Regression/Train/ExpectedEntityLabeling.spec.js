7beddc273041a746f1235c6e0ec02d6eb86ab47b
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-04-25 13:59:31 -0700

    Spec File Rename (#1140)
    - Repathed the root folder that the Regression Tests are in
    - Runs tests based on Glob pattern for the new Regression Test folder
    - Renamed some of the Spec Files
    - Fixed a bug

---
+++ cypress/integration/Regression/Train/ExpectedEntityLabeling.spec.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.  
+ * Licensed under the MIT License.
+*/
+
+import * as models from '../../../support/Models'
+import * as modelPage from '../../../support/components/ModelPage'
+import * as memoryTableComponent from '../../../support/components/MemoryTableComponent'
+import * as scorerModal from '../../../support/components/ScorerModal'
+import * as train from '../../../support/Train'
+import * as common from '../../../support/Common'
+import * as helpers from '../../../support/Helpers'
+
+// This "Expected Entity Labeling" test scenario is Part 1 and
+// the "Learned Entity Labeling" test scenario is Part 2 in that 
+// it continues from where this test case left off by using the
+// model created by this test scenario.
+describe('Expected Entity Labeling - Train', () => {
+  afterEach(helpers.SkipRemainingTestsOfSuiteIfFailed)
+
+  context('Setup', () => {
+    it('Should import a model to test against, navigate to Train Dialogs view and wait for training status to complete', () => {
+      models.ImportModel('z-expectedEntLabl', 'z-whatsYourName.cl')
+      modelPage.NavigateToTrainDialogs()
+      cy.WaitForTrainingStatusCompleted()
+    })
+  })
+
+  context('Train Dialog - 1st Round', () => {
+    it('Should create a new Train Dialog', () => {
+      train.CreateNewTrainDialog()
+    })
+
+    it('Should type in a user utterance and click Score Actions button', () => {
+      train.TypeYourMessage('Hello')
+      train.ClickScoreActionsButton()
+    })
+
+    it('Should verify the Action list contains 1 enabled and 1 disabled Action', () => {
+      scorerModal.VerifyContainsEnabledAction(common.whatsYourName)
+      scorerModal.VerifyContainsDisabledAction('Hello $name')
+    })
+
+    it('Should select an action', () => {
+      train.SelectAction(common.whatsYourName)
+    })
+  })
+
+  context('Train Dialog - 2nd Round', () => {
+    it('Should type in another user utterance, verify it was labeled as the "name" entity and click Score Actions button', () => {
+      train.TypeYourMessage('David')
+      train.VerifyEntityLabel('David', 'name')
+      train.ClickScoreActionsButton()
+    })
+
+    it('Should verify the "name" Entity is in memory with its value', () => {
+      memoryTableComponent.VerifyEntitiesInMemory('name', ['David'])
+    })
+
+    it('Should verify the Action list contains 1 enabled Action and 3 disabled Actions', () => {
+      scorerModal.VerifyContainsDisabledAction(common.whatsYourName)
+      scorerModal.VerifyContainsEnabledAction('Hello David')
+    })
+
+    it('Should select an action', () => {
+      train.SelectAction('Hello David', 'Hello $name')
+    })
+
+    it('Should save the Train Dialog and verify that it shows up in the grid', () => {
+      train.Save()
+    })
+  })
+  // Manually EXPORT this to fixtures folder and name it 'z-expectedEntLabl.cl'
+})
---
+++ cypress/integration/Regression/Train/WaitVsNoWaitActions.spec.js
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.  
+ * Licensed under the MIT License.
+*/
+
+import * as models from '../../../support/Models'
+import * as modelPage from '../../../support/components/ModelPage'
+import * as scorerModal from '../../../support/components/ScorerModal'
+import * as train from '../../../support/Train'
+import * as common from '../../../support/Common'
+import * as helpers from '../../../support/Helpers'
+
+describe('Wait vs Non Wait Actions - Train', () => {
+  afterEach(helpers.SkipRemainingTestsOfSuiteIfFailed)
+
+  context('Setup', () => {
+    it('Should import a model to test against, navigate to Train Dialogs view and wait for training status to complete', () => {
+      models.ImportModel('z-waitNoWait', 'z-waitNoWait.cl')
+      modelPage.NavigateToTrainDialogs()
+      cy.WaitForTrainingStatusCompleted()
+    })
+  })
+
+  context('Train - 1st Round', () => {
+    it('Should create a new Train Dialog', () => {
+      train.CreateNewTrainDialog()
+    })
+
+    it('Should type a user message and click the Score Actions button', () => {
+      train.TypeYourMessage('Hello')
+      train.ClickScoreActionsButton()
+    })
+      
+    it('Should verify all 3 actions are enabled', () => {
+      scorerModal.VerifyContainsEnabledAction('Which animal would you like?')
+      scorerModal.VerifyContainsEnabledAction('Cows say moo!')
+      scorerModal.VerifyContainsEnabledAction(common.ducksSayQuack)
+    })
+
+    it('Should select a wait Action for the Bot Response', () => {
+      train.SelectAction('Which animal would you like?')
+    })
+  })
+
+  context('Train - 2nd Round', () => {
+    it('Should type another user message and click the Score Actions button', () => {
+      train.TypeYourMessage('Cow')
+      train.ClickScoreActionsButton()
+    })
+
+    it('Should verify 3 actions are enabled', () => {
+      scorerModal.VerifyContainsEnabledAction('Which animal would you like?')
+      scorerModal.VerifyContainsEnabledAction('Cows say moo!')
+      scorerModal.VerifyContainsEnabledAction(common.ducksSayQuack)
+    })
+
+    it('Should select a non-wait Action for the Bot Response', () => {
+      train.SelectAction('Cows say moo!')
+    })
+
+    it('Should select a wait Action for the Bot Response which also verifies that the non-wait Action worked as expected', () => {
+      train.SelectAction('Which animal would you like?')
+    })
+  })
+
+  context('Train - 3rd Round', () => {
+    it('Should type another user message and click the Score Actions button', () => {
+      train.TypeYourMessage('Duck')
+      train.ClickScoreActionsButton()
+    })
+
+    it('Should verify 3 actions are enabled', () => {
+      scorerModal.VerifyContainsEnabledAction('Which animal would you like?')
+      scorerModal.VerifyContainsEnabledAction('Cows say moo!')
+      scorerModal.VerifyContainsEnabledAction(common.ducksSayQuack)
+    })
+
+    it('Should select a non-wait Action for the Bot Response', () => {
+      train.SelectAction(common.ducksSayQuack)
+    })
+
+    it('Should select a wait Action for the Bot Response which also verifies that the non-wait Action worked as expected', () => {
+      train.SelectAction('Which animal would you like?')
+    })
+
+    it('Should save the training and verify that it appears in the grid', () => {
+      train.Save()
+    })
+  })
+})
