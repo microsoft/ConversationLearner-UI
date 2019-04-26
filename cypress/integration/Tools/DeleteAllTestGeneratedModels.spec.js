2dfdbaca685ef749199995439d0d5e80af41fbe0
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-04-25 17:43:51 -0700

    fix: should be working - testing

--- cypress/integration/Tools/DeleteAllTestGeneratedModels.spec.js
+++ cypress/integration/Tools/DeleteAllTestGeneratedModels.spec.js
@@ -32,7 +32,7 @@ function DeleteAllTestGeneratedModelRows()
     let thereCouldBeMoreModelsToDelete = false
     modelNameIdList.forEach(modelNameId => 
     {
-      if (modelNameId.name.startsWith('z-')) 
+      if (ModelShouldBeDeleted(modelNameId.name))
       {
         thereCouldBeMoreModelsToDelete = true
         helpers.ConLog(funcName, `Sending Request to Delete Model: ${modelNameId.name}`)
@@ -54,4 +54,47 @@ function DeleteAllTestGeneratedModelRows()
       DeleteAllTestGeneratedModelRows()
     }
   })
+}
+
+function ModelShouldBeDeleted(modelName) {
+  if (!modelName.startsWith('z-')) {
+    // This is NOT a Test created model since it does NOT start with 'z-'
+    return false
+  }
+
+const funcName = `ModelShouldBeDeleted(${modelName})`
+helpers.ConLog(funcName, 'Starts with "z-"')
+
+  // Test created Model names end with a suffix like this...  "-0425-135703x"
+  // The moment format for the suffix is...                   "-MMDD-HHmmss*" where '*' is the Build Key
+  const suffixFormat = '-MMDD-HHmmss*'
+  const suffixLength = suffixFormat.length
+
+  const modelNameSuffix = modelName.substring(modelName.length - suffixLength)
+  if (modelNameSuffix[0] != '-') {
+    // Something is wrong with the format of this model name, 
+    // so to be safe we will not delete it.
+    return false
+  }
+
+helpers.ConLog(funcName, 'Suffix starts with "-"')
+
+  if (modelNameSuffix[suffixLength - 1] == helpers.GetBuildKey()) {
+    // The Build Key in the model matches the Build Key of this test 
+    // run so we can safely delete a model we created.
+    return true
+  }
+
+helpers.ConLog(funcName, 'Key is from another build')
+
+  // This model was created by some other test run, so we need to verify
+  // that the model is too old to still be in use. 5 minutes old is adequate
+  // at this point in time, however, if any test case takes more than 4 minutes
+  // then we should increase this time.
+  const modelCreatedTime = Cypress.moment(modelNameSuffix, suffixFormat)
+  let momentModelIsTooOldToSave = Cypress.moment().subtract(5, 'm')
+
+helpers.ConLog(funcName, `Model is old enough to delete: ${modelCreatedTime.isBefore(momentModelIsTooOldToSave)}`)
+
+  return modelCreatedTime.isBefore(momentModelIsTooOldToSave)
 }
