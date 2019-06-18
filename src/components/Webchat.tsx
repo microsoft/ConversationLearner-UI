3e8fd6813f10e309b124f69db993f99e0e65df2b
Shahin Shayandeh <shahins@microsoft.com>
2019-06-18 13:24:14 -0700

    fix: rely on location.hostname instead of hardcoded localhost (#1187)
    * fix: rely on location.hostname instead of hardcoded localhost
    
    * fix: rename location_host to bot_host_name
    
    * fix: use 443 as the port when protocol is https

--- src/components/Webchat.tsx
+++ src/components/Webchat.tsx
@@ -13,6 +13,7 @@ import { BehaviorSubject, Observable, Subscription } from 'rxjs'
 import { Activity, Message } from 'botframework-directlinejs'
 import { EditDialogType } from './modals/.'
 import actions from '../actions'
+import { BOT_HOST_NAME } from '../types/const'
 
 export function renderActivity(
     activityProps: BotChat.WrappedActivityProps,
@@ -173,7 +174,7 @@ class Webchat extends React.Component<Props, {}> {
             const dl = new BotChat.DirectLine({
                 secret: 'secret',
                 token: 'token',
-                domain: `//localhost:${this.props.settings.botPort}/directline`,
+                domain: `//${BOT_HOST_NAME}:${this.props.settings.botPort}/directline`,
                 webSocket: false // defaults to true,
             })
 
