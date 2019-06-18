c90b87643bf6233b97fc780de255b7d3cfef993f
Shahin Shayandeh <shahins@microsoft.com>
2019-06-18 13:05:57 -0700

    fix: use 443 as the port when protocol is https

--- src/types/const.ts
+++ src/types/const.ts
@@ -24,7 +24,8 @@ export const CL_IMPORT_ID = '9c110735ea8b440d8f31c5c68ffb767d'
 
 export const BOT_HOST_NAME : Readonly<string> = location.hostname
 
-const LOCATION_PORT : string = location.port === '' ? '80' : location.port
+// if location.port is empty, the port will be set to 443 or 80 based on location.protocol
+const LOCATION_PORT : Readonly<string> = location.port === '' ? location.protocol === 'https:' ? '443' : '80' : location.port
 
 export const ports: Readonly<any> = {
     urlBotPort: parseInt(LOCATION_PORT, 10),
