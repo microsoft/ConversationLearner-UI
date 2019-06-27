5aeacec3320ad25969285da120d1f61468734179
Matt Mazzola <mattm@microsoft.com>
2019-06-27 12:58:32 -0700

    fix: import paths

--- cypress/integration/regressionTs/entityPicker.spec.ts
+++ cypress/integration/regressionTs/entityPicker.spec.ts
@@ -1,6 +1,6 @@
-import * as util from '../support/utilities'
-import constants from '../support/constants'
-import s from '../support/selectors'
+import * as util from '../../support/utilities'
+import constants from '../../support/constants'
+import s from '../../support/selectors'
 
 function verifyWordIsLabeled(word: string, entityName: string) {
     cy.get('.cl-entity-node--custom')
