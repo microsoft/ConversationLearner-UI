ea0cf811f44c219f1f01f2ab5a88710b3655dce1
Matt Mazzola <mattm@microsoft.com>
2019-07-17 09:17:59 -0700

    feat: add model header for longer model names

--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -32,47 +32,51 @@ class Settings extends React.Component<Props> {
 
     render() {
         return (
-            <div className="cl-page">
-                <div data-testid="settings-title-2" className={OF.FontClassNames.superLarge}>
-                    <FormattedMessageId id={FM.PROFILE_SETTINGS_TITLE} />
-                </div>
-                <div>
-                    <div className="cl-settings__bot-port">
-                        <div className={`${OF.FontClassNames.xLarge}`}>
-                            <FormattedMessageId id={FM.PROFILE_SETTINGS_BOT_PORT} />
+            <div className="cl-o-app-columns">
+                <div className="cl-app_content">
+                    <div className="cl-page">
+                        <div data-testid="settings-title-2" className={OF.FontClassNames.superLarge}>
+                            <FormattedMessageId id={FM.PROFILE_SETTINGS_TITLE} />
                         </div>
                         <div>
-                            {Util.formatMessageId(this.props.intl, FM.PROFILE_SETTINGS_BOT_PORT_DESCRIPTION, { port: ports.urlBotPort })}
-                        </div>
-                        <div>
-                            <OF.Icon className="cl-icon cl-color-error" iconName="IncidentTriangle" />
-                            <FormattedMessageId id={FM.PROFILE_SETTINGS_BOT_PORT_WARNING} />
-                        </div>
-                        <div data-testid="settings-custom-bot-port">
-                            <OF.Checkbox
-                                label={Util.formatMessageId(this.props.intl, FM.PROFILE_SETTINGS_BOT_PORT_USE_CUSTOM)}
-                                checked={this.props.settings.useCustomPort}
-                                onChange={this.onChangeCustomPort}
-                            />
-                        </div>
-                        <div>
-                            <input
-                                className="cl-input"
-                                type="number"
-                                min={0}
-                                max={99999}
-                                value={this.props.settings.customPort}
-                                onChange={this.onChangeSdkPort}
-                                disabled={!this.props.settings.useCustomPort}
-                            />
+                            <div className="cl-settings__bot-port">
+                                <div className={`${OF.FontClassNames.xLarge}`}>
+                                    <FormattedMessageId id={FM.PROFILE_SETTINGS_BOT_PORT} />
+                                </div>
+                                <div>
+                                    {Util.formatMessageId(this.props.intl, FM.PROFILE_SETTINGS_BOT_PORT_DESCRIPTION, { port: ports.urlBotPort })}
+                                </div>
+                                <div>
+                                    <OF.Icon className="cl-icon cl-color-error" iconName="IncidentTriangle" />
+                                    <FormattedMessageId id={FM.PROFILE_SETTINGS_BOT_PORT_WARNING} />
+                                </div>
+                                <div data-testid="settings-custom-bot-port">
+                                    <OF.Checkbox
+                                        label={Util.formatMessageId(this.props.intl, FM.PROFILE_SETTINGS_BOT_PORT_USE_CUSTOM)}
+                                        checked={this.props.settings.useCustomPort}
+                                        onChange={this.onChangeCustomPort}
+                                    />
+                                </div>
+                                <div>
+                                    <input
+                                        className="cl-input"
+                                        type="number"
+                                        min={0}
+                                        max={99999}
+                                        value={this.props.settings.customPort}
+                                        onChange={this.onChangeSdkPort}
+                                        disabled={!this.props.settings.useCustomPort}
+                                    />
 
-                            <OF.PrimaryButton
-                                text="Reset"
-                                ariaDescription="Reset"
-                                iconProps={{ iconName: 'Undo' }}
-                                onClick={this.reset}
-                                disabled={!this.props.settings.useCustomPort}
-                            />
+                                    <OF.PrimaryButton
+                                        text="Reset"
+                                        ariaDescription="Reset"
+                                        iconProps={{ iconName: 'Undo' }}
+                                        onClick={this.reset}
+                                        disabled={!this.props.settings.useCustomPort}
+                                    />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
