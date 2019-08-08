4ad28cc67a0a7354fba23725a2a231c9a9a525c0
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-08-07 19:11:18 -0700

    fix: working fine tuned action scorer verification code

--- cypress/support/components/ScorerModal.js
+++ cypress/support/components/ScorerModal.js
@@ -90,7 +90,6 @@ export function VerifyScoreActions(expectedScoreActions) {
   let expectedScoreAction
   let errorMessages = []
   let rowIndex = 0
-  let rowElements
 
   function AccumulateErrors(message) {
     const fullMessage = `Row: ${rowIndex} - Response: ${expectedScoreAction.response} - ${message}`
@@ -98,10 +97,10 @@ export function VerifyScoreActions(expectedScoreActions) {
     helpers.ConLog(funcName, fullMessage)
   }
 
-  function SelectAndVerify(selectionCommand, verificationFunction) {
-    let elements = eval(selectionCommand)
-    if (elements.length != 1) { 
-      AccumulateErrors(`Expected to find 1 and only 1 instead we found ${elements.length} - Selection Command: ${selectionCommand}`)
+  function FindWithinAndVerify(baseElements, findCommand, verificationFunction, expectedElementCount = 1) {
+    let elements = eval(`Cypress.$(baseElements).${findCommand}`)
+    if (elements.length != expectedElementCount) { 
+      AccumulateErrors(`Expected to find exactly ${expectedElementCount} element(s) instead we found ${elements.length} - Selection Command: ${findCommand}`)
     } else { 
       verificationFunction(elements) 
     }
@@ -137,108 +136,75 @@ export function VerifyScoreActions(expectedScoreActions) {
         AccumulateErrors(rowElementsOrErrorMessage)
         continue
       }
-      rowElements = rowElementsOrErrorMessage
-      helpers.ConLog(funcName, `Element found: ${rowElements[0].outerHTML}`)
+      let rowElement = rowElementsOrErrorMessage[0]
+      helpers.ConLog(funcName, `Element found: ${rowElement.outerHTML}`)
 
       // We use the rowIndex only for the purpose of logging errors as a debugging aid.
-      rowIndex = Cypress.$(rowElements[0]).parents('div[role="presentation"].ms-List-cell').attr('data-list-index')
+      rowIndex = Cypress.$(rowElement).parents('div[role="presentation"].ms-List-cell').attr('data-list-index')
+      
       
       // Verify the button.
-      let elements
-      SelectAndVerify(`Cypress.$(rowElements[0]).find('[data-testid^="action-scorer-button-"]')`, elements => {
+      FindWithinAndVerify(rowElement, `find('[data-testid^="action-scorer-button-"]')`, elements => {
         let attr = elements.attr('data-testid')
         if (attr != expectedButtonTestId) {
           AccumulateErrors(`Expected to find data-testid="${expectedButtonTestId}" instead we found "${attr}"`)
         }
       })
 
-      //let elements = Cypress.$(rowElements[0]).find('[data-testid^="action-scorer-button-"]')
-      // eval(`elements = Cypress.$(rowElements[0]).find('[data-testid^="action"]')`)
-      // if (elements.length != 1) { 
-      //   AccumulateErrors(`Expected to find 1 and only 1 data-testid starting with "action-scorer-button-", instead we found ${elements.length}`)
-      // } else {
-      //   let attr = elements.attr('data-testid')
-      //   if (attr != expectedButtonTestId) {
-      //     AccumulateErrors(`Expected to find data-testid="${expectedButtonTestId}" instead we found "${attr}"`)
-      //   }
-      // }
-
       
       // Verify the score.
-      SelectAndVerify(`Cypress.$(rowElements[0]).find('[data-testid="action-scorer-score"]')`, elements => {
+      FindWithinAndVerify(rowElement, `find('[data-testid="action-scorer-score"]')`, elements => {
         let score = helpers.TextContentWithoutNewlines(elements[0])
         if (score != expectedScore) {
           AccumulateErrors(`Expected to find a score with '${expectedScore}' but instead found this '${score}'`)
         }
       })
 
-      // elements = Cypress.$(rowElements[0]).find('[data-testid="action-scorer-score"]')
-      // if (elements.length != 1) { 
-      //   AccumulateErrors(`Expected to find 1 and only 1 data-testid with "action-scorer-score", instead we found ${elements.length}`)
-      // } else {
-      //   let score = helpers.TextContentWithoutNewlines(elements[0])
-      //   if (score != expectedScore) {
-      //     AccumulateErrors(`Expected to find a score with '${expectedScore}' but instead found this '${score}'`)
-      //   }
-      // }
-
       
       // Verify the entities.
-      elements = Cypress.$(rowElements[0]).find('[data-testid="action-scorer-entities"]').parent('div[role="listitem"]')
-      if (elements.length != expectedScoreAction.entities.length) { 
-        AccumulateErrors(`Expected to find ${expectedScoreAction.entities.length} entities but instead we found ${elements.length}`)
-      }
-      expectedScoreAction.entities.forEach(entity => {
-        let entityElement = Cypress.$(elements).find(`[data-testid="action-scorer-entities"]:contains("${entity.name}")`)
-        if (entityElement.length != 1) {
-          AccumulateErrors(`Expected to find 1 and only 1 "action-scorer-entities" named "${entity.name}" instead we found ${entityElement.length}`)
-        } else {
-          let strikeOut = Cypress.$(entityElement).find(`del:contains("${entity.name}")`).length == 1 ? 'Strikeout' : ''
-          let entityQualifierState
+      FindWithinAndVerify(rowElement, `find('[data-testid="action-scorer-entities"]').parent('div[role="listitem"]')`, elements => {
+        expectedScoreAction.entities.forEach(entity => {
+          FindWithinAndVerify(elements, `find('[data-testid="action-scorer-entities"]:contains("${entity.name}")')`, entityElement => {
+            let strikeOut = Cypress.$(entityElement).find(`del:contains("${entity.name}")`).length == 1 ? 'Strikeout' : ''
+            let entityQualifierState
+  
+            if (entityElement.hasClass('cl-entity--match')) {
+              entityQualifierState = entityQualifierStateEnum.green + strikeOut
+            } else if (entityElement.hasClass('cl-entity--mismatch')) {
+              entityQualifierState = entityQualifierStateEnum.red + strikeOut
+            } else {
+              AccumulateErrors(`Expected to find class with either 'cl-entity--match' or 'cl-entity--mismatch' but found neither. Element: ${entityElement[0].outerHTML}`)
+            }
+  
+            if (entity.qualifierState != entityQualifierState) {
+              AccumulateErrors(`Expected '${entity.name}' Entity Qualifier to have State: ${entity.qualifierState} but instead found: ${entityQualifierState}`)
+            }
+          })
+        })
+      }, expectedScoreAction.entities.length)
 
-          if (entityElement.hasClass('cl-entity--match')) {
-            entityQualifierState = entityQualifierStateEnum.green + strikeOut
-          } else if (entityElement.hasClass('cl-entity--mismatch')) {
-            entityQualifierState = entityQualifierStateEnum.red + strikeOut
-          } else {
-            AccumulateErrors(`Expected to find class with either 'cl-entity--match' or 'cl-entity--mismatch' but found neither. Element: ${entityElement[0].outerHTML}`)
-          }
-
-          if (entity.qualifierState != entityQualifierState) {
-            AccumulateErrors(`Expected '${entity.name}' Entity Qualifier to have State: ${entity.qualifierState} but instead found: ${entityQualifierState}`)
-          }
-        }
-      })
-
-            
+      
       // Verify the Wait flag.
-      elements = Cypress.$(rowElements[0]).find('[data-testid="action-scorer-wait"]')
-      if (elements.length != 1) { 
-        AccumulateErrors(`Expected to find 1 and only 1 data-testid with "action-scorer-wait", instead we found ${elements.length}`)
-      } else {
+      FindWithinAndVerify(rowElement, `find('[data-testid="action-scorer-wait"]')`, elements => {
         let wait = elements.attr('data-icon-name') == 'CheckMark'
         if (wait != expectedScoreAction.wait) {
           AccumulateErrors(`Expected to find Wait: '${expectedScoreAction.wait}' but instead it was: '${wait}'`)
         }
-      }
+      })
 
 
       // Verify the Action Type.
-      elements = Cypress.$(rowElements[0]).find('[data-testid="action-details-action-type"]')
-      if (elements.length != 1) { 
-        AccumulateErrors(`Expected to find 1 and only 1 data-testid with "action-details-action-type", instead we found ${elements.length}`)
-      } else {
+      FindWithinAndVerify(rowElement, `find('[data-testid="action-details-action-type"]')`, elements => {
         let actionType = helpers.TextContentWithoutNewlines(elements[0])
         if (actionType != expectedScoreAction.type) {
           AccumulateErrors(`Expected to find Action Type: '${expectedScoreAction.type}' but instead it was: '${actionType}'`)
         }
-      }
+      })
     }
     
-    if (errorMessages.length > 0) {throw new Error(`${errorMessages.length} Errors Detected in Action Scorer Grid- See log file for full list. - 1st Error: ${errorMessages[0]}`)}    
+    if (errorMessages.length > 0) {throw new Error(`${errorMessages.length} Errors Detected in Action Scorer Grid - See log file for full list. --- 1st Error: ${errorMessages[0]}`)}    
   })
 }
 
 export const stateEnum = { selected: 1, qualified: 2, disqualified: 3 }
-//export const entityQualifierStateEnum = { unknown: 0, green: 1, greenStrikeout: 2, red: 3, redStrikeout: 4 }
 export const entityQualifierStateEnum = { unknown: 'unknown', green: 'Green', greenStrikeout: 'GreenStrikeout', red: 'Red', redStrikeout: 'RedStrikeout' }
