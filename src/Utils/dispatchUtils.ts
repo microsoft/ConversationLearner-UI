eb337381d9add653d1283bd5bfb02dc129ee78ab
Matt Mazzola <mattm@microsoft.com>
2019-08-29 11:44:19 -0500

    chore: refactor dispatch algorithm

--- src/Utils/dispatchUtils.ts
+++ src/Utils/dispatchUtils.ts
@@ -4,6 +4,11 @@ import * as uuid from 'uuid/v4'
 import * as Util from './util'
 import { DispatcherAlgorithmType } from 'src/components/modals/DispatcherCreator';
 
+/**
+ * Generations new source based on child sources and given algorithm
+ * @param sourceModelPairs Model Sources and Model Definitions Paired with each other.
+ * @param algorithmType Type of generation Algorithm
+ */
 export function generateDispatcherSource(
     sourceModelPairs: SourceAndModelPair[],
     algorithmType: DispatcherAlgorithmType,
@@ -16,17 +21,37 @@ export function generateDispatcherSource(
     throw new Error(`Could not associate Dispatcher Algorithm Type with algorithm. You passed: ${algorithmType}`)
 }
 
+/**
+ * Attempt to transition at first N number of rounds for each dialog in model, to a dialog in another model.
+ * @param transitionRoundLimit Limit on number of places to transfer between models. Defaults to 3
+ */
 function generageDeterministicDispatcherSource(
     sourceModelPairs: SourceAndModelPair[],
     transitionRoundLimit: number = 3,
 ): CLM.AppDefinition {
-    /**
-     * Generate 1 Dispatch Action per model and associate with source + model pair
-     *
-     * Store:
-     * - modelId for dispatching
-     * - modelName for display
-     */
+    generateDispatchActions(sourceModelPairs);
+
+    const modelsTrainDialogs = associateModelDialogsWithDispatchActionsAndClean(sourceModelPairs)
+
+    const trainDialogs = determisticSingleTransfer(modelsTrainDialogs, transitionRoundLimit);
+
+    const source: CLM.AppDefinition = {
+        trainDialogs,
+        actions: sourceModelPairs.map(sm => sm.action),
+        entities: [],
+    }
+
+    return source
+}
+
+/**
+ * Generate 1 Dispatch Action per model and associate with source + model pair
+ *
+ * Store:
+ * - modelId for dispatching
+ * - modelName for display
+ */
+function generateDispatchActions(sourceModelPairs: SourceAndModelPair[]) {
     sourceModelPairs.forEach(smp => {
         // If object already has action associated, don't create new one
         if (smp.action) {
@@ -57,16 +82,18 @@ function generageDeterministicDispatcherSource(
 
         smp.action = dispatchAction
     })
+}
 
-    /**
-     * For each dialog in model A set each rounds to use that models Dispatch Action
-     * This means, when this input (extraction) is seen, dispatch to this model.
-     *
-     * Clear all entities, and ensure single scorer step
-     */
-    const modelTrainDialogs = sourceModelPairs.map((sm, mIndex) => {
+/**
+ * For each dialog in model A set each rounds to use that models Dispatch Action
+ * This means, when this input (extraction) is seen, dispatch to this model.
+ *
+ * Clear all entities, and ensure single scorer step
+ */
+function associateModelDialogsWithDispatchActionsAndClean(sourceModelPairs: SourceAndModelPair[]): CLM.TrainDialog[][] {
+    return sourceModelPairs.map((sm, mIndex) => {
         if (!sm.action) {
-            throw new Error(`(Source + Model) pair must have dispatch action assigned at this point`)
+            throw new Error(`(Source + Model) pair must have dispatch action assigned at this point`);
         }
 
         return sm.source.trainDialogs.map((t, tIndex) => {
@@ -93,48 +120,50 @@ function generageDeterministicDispatcherSource(
                 ]
             })
 
-            t.tags = [`model-${mIndex + 1}`, `dialog-${tIndex + 1}`]
-            // t.description = `Model: ${sm.model.appName} - Dialog ${tIndex + 1}`
+            t.tags = [`model-${mIndex + 1}`, `dialog-${tIndex + 1}`];
+            // t.description = `Model: ${sm.model.appName} - Dialog ${tIndex + 1}
 
-            return t
-        })
-    })
+            return t;
+        });
+    });
+}
 
-    /**
-     * Intermix rounds from different dialogs to implicitly demonstrate dispatching/context switching to other model
-     *
-     * Example
-     * Dialogs:
-     *  ModelA:
-     *   [A,B,C]
-     *  ModelB:
-     *   [D,E,F]
-     *  ModelC:
-     *   [G,H,I]
-     * ...
-     *
-     * Output:
-     * [
-     *  [A,D,E,F],
-     *  [A,B,D,E,F],
-     *  [A,B,C,D,E,F],
-     *  [A,G,H,I],
-     *  [A,B,G,H,I],
-     *  [A,B,C,G,H,I],
-     *  [D,A,B,C],
-     *  [D,E,A,B,C],
-     *  [D,E,F,A,B,C],
-     *  [D,G,H,I],
-     *  [D,E,G,H,I],
-     *  [D,E,F,G,H,I],
-     *  ...
-     * ]
-     */
+/**
+ * Intermix rounds from different dialogs to implicitly demonstrate dispatching/context switching to other model
+ *
+ * Example
+ * Dialogs:
+ *  ModelA:
+ *   [A,B,C]
+ *  ModelB:
+ *   [D,E,F]
+ *  ModelC:
+ *   [G,H,I]
+ * ...
+ *
+ * Output:
+ * [
+ *  [A,D,E,F],
+ *  [A,B,D,E,F],
+ *  [A,B,C,D,E,F],
+ *  [A,G,H,I],
+ *  [A,B,G,H,I],
+ *  [A,B,C,G,H,I],
+ *  [D,A,B,C],
+ *  [D,E,A,B,C],
+ *  [D,E,F,A,B,C],
+ *  [D,G,H,I],
+ *  [D,E,G,H,I],
+ *  [D,E,F,G,H,I],
+ *  ...
+ * ]
+ */
 
-    // Flatten dialogs from models
-    // TODO: This could cause dispatch model to train on dispatching to dialogs within itself.
-    // This could be good, but also could be extra noise.
-    const allTrainDialogs = modelTrainDialogs
+function determisticSingleTransfer(
+    modelsTrainDialogs: CLM.TrainDialog[][],
+    transitionRoundLimit: number
+): CLM.TrainDialog[] {
+    const allTrainDialogs = modelsTrainDialogs
         .reduce((a, b) => [...a, ...b])
 
     const dialogsWithoutModelTransition = Util.deepCopy(allTrainDialogs)
@@ -145,54 +174,67 @@ function generageDeterministicDispatcherSource(
      * Could try re-entry patterns: A B A
      * Could try multi model switch: A B C
      */
-    const mixRounds = allTrainDialogs.map(t => t.rounds)
-        .map((rs, i, allDialogsRounds) => {
-            // Get rounds from dialogs other than the one being dispatched
-            const otherDialogsRounds = allDialogsRounds.filter((_, j) => j !== i)
+    const mixRounds = modelsTrainDialogs
+        .map((modelDialogs, mIndex) => {
 
-            // TODO: Adapt to be able to transition at multiple points in dialog
-            // Beginning (First 4) and End (Last 4) ?
-            const possibleTransitionRounds = rs
-                // Only attempt to transition within first 4 rounds
-                // After 4, assume user will stay on task
-                .filter((_, j) => j <= transitionRoundLimit)
+            /**
+             * Original implementation attempted transitions within same model and preserved action
+             * This could be good or introduce noise. Consider it as option
+             */
+            const trainDialogsFromOtherModels = modelsTrainDialogs.filter((_, k) => k !== mIndex);
+            return modelDialogs
+                .map(t => t.rounds)
+                .map((rs, i, allDialogsRounds) => {
+                    // Get rounds from dialogs other than the one being dispatched
+                    const otherDialogsRounds = trainDialogsFromOtherModels
+                        .reduce((a, b) => [...a, ...b])
+                        .map(t => t.rounds)
 
-            // For each round, try to transition to one of the other dialogs
-            return otherDialogsRounds
-                .map((dialogRounds) => {
-                    return possibleTransitionRounds
-                        .map((_, k) => {
-                            // Get rounds until index
-                            const firstRounds = rs.slice(0, k + 1)
-                            // Note: always concatenates entire dialog from start
-                            const secondRounds = dialogRounds
-                            return [...firstRounds, ...secondRounds]
+                    // TODO: Adapt to be able to transition at multiple points in dialog
+                    // Beginning (First 4) and End (Last 4) ?
+                    const possibleTransitionRounds = rs
+                        // Only attempt to transition within first 4 rounds
+                        // After 4, assume user will stay on task
+                        .filter((_, j) => j <= transitionRoundLimit);
+
+                    // For each round, try to transition to one of the other dialogs
+                    return otherDialogsRounds
+                        .map((dialogRounds) => {
+                            return possibleTransitionRounds
+                                .map((_, k) => {
+                                    // Get rounds until index
+                                    const firstRounds = rs.slice(0, k + 1)
+                                    // Note: always concatenates entire dialog from start
+                                    const secondRounds = dialogRounds
+                                    return [...firstRounds, ...secondRounds]
+                                });
                         })
+                        .reduce((a, b) => [...a, ...b])
                 })
                 .reduce((a, b) => [...a, ...b])
         })
         .reduce((a, b) => [...a, ...b])
 
-    const mixedDialogs = mixRounds.map(rs => ({
-        tags: [`generated`],
-        description: "",
-        trainDialogId: uuid(),
-        rounds: rs,
-        clientData: {
-            importHashes: []
-        },
-        initialFilledEntities: [],
-    }))
+    const mixedDialogs = mixRounds.map<CLM.TrainDialog>(rs => {
+        return {
+            tags: [`generated`],
+            description: "",
+            trainDialogId: uuid(),
+            rounds: rs,
 
-    const source = {
-        trainDialogs: [
-            ...dialogsWithoutModelTransition,
-            ...mixedDialogs
-        ],
-        actions: sourceModelPairs.map(sm => sm.action),
-        entities: [],
-        packageId: uuid()
-    }
+            // Ignored fields
+            clientData: {
+                importHashes: []
+            },
+            initialFilledEntities: [],
+            createdDateTime: new Date().toJSON(),
+            lastModifiedDateTime: new Date().toJSON(),
+        } as unknown as CLM.TrainDialog
+    })
 
-    return source as CLM.AppDefinition
+    return [
+        ...dialogsWithoutModelTransition,
+        ...mixedDialogs
+    ]
 }
+
