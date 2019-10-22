c7beb21e59cd31d5dcf095800c1607307a1ecabb
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-10-21 21:42:39 -0700

    fix: backup

---
+++ cypress/integration/zTemp/EntityLabelingTemp.spec.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.  
+ * Licensed under the MIT License.
+*/
+
+import * as models from '../../support/Models'
+import * as modelPage from '../../support/components/ModelPage'
+import * as train from '../../support/Train'
+import * as trainDialogsGrid from '../../support/components/TrainDialogsGrid'
+import * as common from '../../support/Common'
+import * as helpers from '../../support/Helpers'
+
+describe('Consistent Entity Labeling', () => {
+  const textEntityPairs = [{ text: 'Tag', entity: 'multi' }, { text: 'Frog', entity: 'multi' }]
+
+  afterEach(helpers.SkipRemainingTestsOfSuiteIfFailed)
+  
+  context('Setup', () => {
+    it('Import a model, wait for training to complete and start a new Train Dialog', () => {
+      models.ImportModel('z-temp', 'temp-cnstntEntLabel.cl')
+      modelPage.NavigateToTrainDialogs()
+      cy.WaitForTrainingStatusCompleted()
+      train.CreateNewTrainDialog()
+    })
+  })
+
+  context('Edit and Preserve Attempted Labels', () => {
+    it('Edit the Train Dialog that got changed', () => {
+      train.EditTraining('This is Tag.', 'This is Tag.', 'Hi')
+      cy.wait(5000)
+    })
+
+    it('Select the user turn and verify there is an error message', () => {
+      train.SelectChatTurnExactMatch('This is Tag.')
+    })
+
+    it('Re-lable the entities to set things right in this Train Dialog', () => {
+      train.XLabelTextAsEntity('Frog', 'multi', 1)
+      train.XLabelTextAsEntity('Tag', 'multi', 1)
+    })
+
+    it('', () => {
+    })
+
+    it('', () => {
+    })
+  })
+})
