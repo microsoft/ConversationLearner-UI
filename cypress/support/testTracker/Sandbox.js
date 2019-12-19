5fce0827a03e4adce53ca2c3d7b5a6b7fd44e9f9
Michael Skowronski <15230446+SDESkowronski@users.noreply.github.com>
2019-12-18 18:00:15 -0800

    fix: removing the entier folder so tests will run hopefully

--- cypress/support/testTracker/Sandbox.js
+++
@@ -1,7 +0,0 @@
-const apiData = require('./ApiData');
-
-(async function() {
-  return await apiData.Get('https://dialearn.visualstudio.com/BLIS/_workitems/edit/2422').then(data => {
-    console.log(data)
-  })
-}())
